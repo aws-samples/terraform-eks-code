0c44c873eac672efa2ff3cacec2415ad4e16c3fc
awsandy <awsandy@amazon.co.uk>
2020-10-10 15:48:44 +0100

    private rtb

--- vpce/data_aws_rtbs.tf
+++ vpce/data_aws_rtbs.tf
@@ -1,8 +1,8 @@
 data "aws_route_tables" "rts" {
   vpc_id = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
 
-  #filter {
-  #  name   = "tag:kubernetes.io/kops/role"
-  #  values = ["private*"]
-  #}
+  filter {
+    name   = "tag:Name"
+    values = ["*Private*"]
+  }
 }
