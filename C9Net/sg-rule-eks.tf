ae78ea29f6de20c055951f42d04bc12d2d735845
awsandy <awsandy@amazon.co.uk>
2020-10-30 18:59:31 +0000

    sg rule

---
+++ C9Net/sg-rule-eks.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group_rule" "eks-all" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [data.aws_vpc.vpc-default.cidr_block]
+  security_group_id = data.terraform_remote_state.net.outputs.cluster-sg
+}
+
+resource "aws_security_group_rule" "eks-node" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [data.aws_vpc.vpc-default.cidr_block]
+  security_group_id = data.terraform_remote_state.net.outputs.allnodes-sg
+}
