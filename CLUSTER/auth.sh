4bb38c76b6093d16c71cfadab8051515a2dd677b
awsandy <awsandy@amazon.co.uk>
2020-10-31 17:45:42 +0000

    auth

--- CLUSTER/auth.sh
+++ CLUSTER/auth.sh
@@ -1,2 +1,3 @@
+cn=`terraform output`
 aws eks update-kubeconfig --name mycluster1
 kubectx
---
+++ setup-disk.sh
@@ -0,0 +1,30 @@
+pip install --user --upgrade boto3
+export instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+python -c "import boto3
+import os
+from botocore.exceptions import ClientError 
+ec2 = boto3.client('ec2')
+volume_info = ec2.describe_volumes(
+    Filters=[
+        {
+            'Name': 'attachment.instance-id',
+            'Values': [
+                os.getenv('instance_id')
+            ]
+        }
+    ]
+)
+volume_id = volume_info['Volumes'][0]['VolumeId']
+try:
+    resize = ec2.modify_volume(    
+            VolumeId=volume_id,    
+            Size=30
+    )
+    print(resize)
+except ClientError as e:
+    if e.response['Error']['Code'] == 'InvalidParameterValue':
+        print('ERROR MESSAGE: {}'.format(e))"
+if [ $? -eq 0 ]; then
+    sudo reboot
+fi
+
