eb430da68046dde1526f77e8f2498c6b62931dae
awsandy <awsandy@amazon.co.uk>
2020-11-01 18:26:15 +0000

    clean

---
+++ 05-NODEGROUP-test/cleanup.sh
@@ -0,0 +1,10 @@
+cd 05*
+terraform destroy -auto-approve
+cd 04*
+terraform destroy -auto-approve
+cd 03*
+terraform destroy -auto-approve
+cd 02*
+terraform destroy -auto-approve
+cd 01*
+terraform destroy -auto-approve
