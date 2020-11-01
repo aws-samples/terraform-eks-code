2b3a47281f97c53a320e52737fc5003e4be14c04
awsandy <awsandy@amazon.co.uk>
2020-11-01 17:05:17 +0000

    up

--- 04-CLUSTER/auth.sh
+++ 04-CLUSTER/auth.sh
@@ -1,5 +1,6 @@
+rm -f ~/.kube/config
 cn=`terraform output cluster-name`
 arn=$(aws sts get-caller-identity | jq -r .Arn)
 aws eks update-kubeconfig --name $cn
-aws eks update-kubeconfig --name $cn  --role-arn $arn
+#aws eks update-kubeconfig --name $cn  --role-arn $arn
 kubectx
