b701f4f1de73c4b2d950af2b3384ba8c478e457c
awsandy <awsandy@amazon.co.uk>
2020-11-11 15:26:40 +0000

    up

---
+++ tf-setup/s3-bucket.tf
@@ -0,0 +1,32 @@
+data "external" "bucket_name" {
+  program = ["bash", "get-bucket-name.sh"]
+}
+
+output "Name" {
+  value = data.external.bucket_name.result.Name
+}
+
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = data.external.bucket_name.result.Name
+
+  // This is only here so we can destroy the bucket as part of automated tests. You should not copy this for production
+  // usage
+  force_destroy = true
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
