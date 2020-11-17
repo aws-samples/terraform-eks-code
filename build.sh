ebfe7b55abc9d6d785b4287efe7fa6b30a5ec060
awsandy <awsandy@amazon.co.uk>
2020-11-17 20:43:12 +0000

    build

--- build.sh
+++ build.sh
@@ -2,37 +2,10 @@ cur=`pwd`
 dirs="tf-setup net iam c9net cluster nodeg"
 for i in $dirs; do
 cd $i
-pwd
-cd $cur
-done
-exit
-
-cd ..
+echo "Building in $i"
 terraform init -no-color
 terraform plan -out tfplan -no-color
 terraform apply tfplan -no-color
-cd net
-terraform init
-terraform plan -out tfplan
-terraform apply tfplan
-cd ..
-cd iam
-terraform init
-terraform plan -out tfplan
-terraform apply tfplan
-cd ..
-cd c9net
-terraform init
-terraform plan -out tfpan
-terraform apply tfplan
-cd ..
-cd cluster
-terraform init
-terraform plan -out tfpan
-terraform apply tfplan
-cd ..
-cd nodeg
-terraform init
-terraform plan -out tfpan
-terraform apply tfplan
-cd ..
+cd $cur
+done
+exit
