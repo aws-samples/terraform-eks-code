171bbe357736de9f613b4dabeb0e7b58db46c632
awsandy <awsandy@amazon.co.uk>
2020-11-18 16:54:46 +0000

    up

--- nodeg/null_resource.tf
+++ nodeg/null_resource.tf
@@ -2,12 +2,12 @@ resource "null_resource" "auth_cluster" {
 triggers = {
     always_run = "${timestamp()}"
 }
-depends_on = [aws_eks_cluster.eks_cluster]
+depends_on = [data.aws_eks_cluster.eks_cluster]
 provisioner "local-exec" {
     on_failure  = fail
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        echo -e "\x1B[31m Warning! Checking Authorization ${aws_eks_cluster.eks_cluster.name}...should see Server Version: v1.17.xxx \x1B[0m"
+        echo -e "\x1B[31m Warning! Checking Authorization ${data.aws_eks_cluster.eks_cluster.name}...should see Server Version: v1.17.xxx \x1B[0m"
         ./auth.sh
         echo "************************************************************************************"
      EOT
