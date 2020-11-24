ea34aa90bd5c125546fc3e0d3d59ba8512c28c90
awsandy <awsandy@amazon.co.uk>
2020-11-24 18:30:22 +0000

    sample app

---
+++ sampleapp/app.sh
@@ -0,0 +1,3 @@
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/examples/2048/2048_full.yaml
+wget https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/examples/2048/2048_full.yaml
+docker pull alexwhen/docker-2048
