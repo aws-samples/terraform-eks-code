dbc35f6d88666575224880131eca1671150cdd39
awsandy <awsandy@amazon.co.uk>
2020-11-26 20:28:24 +0000

    up

--- eks-cidr/02.sh
+++ eks-cidr/02.sh
@@ -9,8 +9,16 @@ fi
 kubectl set env ds aws-node -n kube-system AWS_VPC_K8S_CNI_CUSTOM_NETWORK_CFG=true
 kubectl describe daemonset aws-node -n kube-system | grep -A5 Environment
 INSTANCE_IDS=(`aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --filters "Name=tag-key,Values=eks:cluster-name" "Name=instance-state-name,Values=running" "Name=tag-value,Values=$CLUSTER" --output text` )
+target=$(kubectl get nodes | grep Read | wc -l)
+curr=0
 for i in "${INSTANCE_IDS[@]}"
 do
-echo "Terminating EC2 instance $i ..."
+echo "Terminating EC2 instance $i ... "
 aws ec2 terminate-instances --instance-ids $i
+while [ $cur -ne $target ]; do
+    sleep 5
+    curr=$(kubectl get nodes | grep Ready | wc -l)
+    kubectl get nodes
+    echo "Current Ready nodes = $curr of $target"
+done
 done
