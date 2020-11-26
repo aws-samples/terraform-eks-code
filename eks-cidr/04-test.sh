b664dddb05ddf23d0411e2b6480dbd229d3f5a05
awsandy <awsandy@amazon.co.uk>
2020-11-26 16:59:04 +0000

    up

--- eks-cidr/04-test.sh
+++ eks-cidr/04-test.sh
@@ -2,6 +2,6 @@ kubectl create deployment nginx --image=566972129213.dkr.ecr.eu-west-1.amazonaws
 kubectl scale --replicas=3 deployments/nginx
 kubectl expose deployment/nginx --type=NodePort --port 80
 kubectl get pods -o wide
-echo "kubectl run -i --rm --tty debug --image=busybox -- sh"
+echo "kubectl run -i --rm --tty debug --image=566972129213.dkr.ecr.eu-west-1.amazonaws.com/busybox -- sh"
 echo "wget google.com -O -"
 echo "wget nginx -O -"
