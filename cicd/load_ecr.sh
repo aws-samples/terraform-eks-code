1b71065c38be8a0397cb4790b6fee650d48853f4
awsandy <awsandy@amazon.co.uk>
2020-11-27 12:20:50 +0000

    up

---
+++ cicd/load_ecr.sh
@@ -0,0 +1,16 @@
+test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || "echo AWS_REGION is not set && exit"
+test -n "$ACCOUNT_ID" && echo ACCOUNT_ID is "$ACCOUNT_ID" || "echo ACCOUNT_ID is not set && exit"
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+#docker pull alexwhen/docker-2048
+#docker tag docker-2048 aws_account_id.dkr.ecr.region.amazonaws.com/docker-2048
+dirs="nginx busybox"
+for i in $dirs; do
+docker pull $i
+docker tag $i $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$i
+docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$i
+done
+
+docker pull alexwhen/docker-2048 
+docker tag alexwhen/docker-2048 $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/sample-app
+docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/sample-app
+
---
+++ cicd/null-load_ecr.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "load_ecr" {
+triggers = {
+    always_run = "${timestamp()}"
+}
+depends_on = [aws_ecr_repository.busybox]
+provisioner "local-exec" {
+    on_failure  = fail
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOT
+        ./load_ecr.sh
+        echo "************************************************************************************"
+     EOT
+}
+}
