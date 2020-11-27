b8d9a589b8097f7cb7e937222a18469841dbe0cb
awsandy <awsandy@amazon.co.uk>
2020-11-27 11:23:40 +0000

    up

--- cluster/null_resource.tf
+++ cluster/null_resource.tf
@@ -7,9 +7,9 @@ provisioner "local-exec" {
     on_failure  = fail
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        echo -e "\x1B[31m Warning! Testing Network Connectivity ${aws_eks_cluster.mycluster1.name}...should see port 443/tcp open  https\x1B[0m"
+        echo -e "\x1B[32m Testing Network Connectivity ${aws_eks_cluster.mycluster1.name}...should see port 443/tcp open  https\x1B[0m"
         ./test.sh ${aws_eks_cluster.mycluster1.name}
-        echo -e "\x1B[31m Warning! Checking Authorization ${aws_eks_cluster.mycluster1.name}...should see Server Version: v1.17.xxx \x1B[0m"
+        echo -e "\x1B[32m Checking Authorization ${aws_eks_cluster.mycluster1.name}...should see Server Version: v1.17.xxx \x1B[0m"
         ./auth.sh ${aws_eks_cluster.mycluster1.name}
         echo "************************************************************************************"
      EOT
