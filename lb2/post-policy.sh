a8d5c57b9585f370c0e9176581ddfc502fad18cc
awsandy <awsandy@amazon.co.uk>
2020-11-27 16:54:24 +0000

    up

--- lb2/post-policy.sh
+++ lb2/post-policy.sh
@@ -5,4 +5,6 @@ eksctl utils associate-iam-oidc-provider   --region $1 --cluster $2  --approve
 eksctl create iamserviceaccount --cluster mycluster1 --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::$3:policy/AWSLoadBalancerControllerIAMPolicy --approve
 #helm repo add eks https://aws.github.io/eks-charts
 kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
-helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=$2 --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller --set image.repository=602401143452.dkr.ecr.$1.amazonaws.com/amazon/aws-load-balancer-controller 
+helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=$2 --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller --set image.repository=602401143452.dkr.ecr.$1.amazonaws.com/amazon/aws-load-balancer-controller
+sleep 3
+kubectl get pods -A  | grep aws-load-balancer-controller
