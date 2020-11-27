38f417e87c0afc33800ac441a11ec9c9369a042b
awsandy <awsandy@amazon.co.uk>
2020-11-27 16:47:09 +0000

    up

--- lb2/null_post_policy.tf
+++ lb2/null_post_policy.tf
@@ -10,16 +10,9 @@ provisioner "local-exec" {
         carn=$(echo ${data.aws_eks_cluster.eks_cluster.arn})
         reg=$(echo $carn | cut -f4 -d':')
         acc=$(echo $carn | cut -f5 -d':')
-        echo "$reg $acc"
-        eksctl utils associate-iam-oidc-provider   --region $reg --cluster ${data.aws_eks_cluster.eks_cluster.name}  --approve
-        # helm chart does this if you let it
-        eksctl create iamserviceaccount --cluster mycluster1 --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::${acc}:policy/AWSLoadBalancerControllerIAMPolicy --approve
-        #helm repo add eks https://aws.github.io/eks-charts
-
-        kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
-
-        helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=${data.aws_eks_cluster.eks_cluster.name} --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller --set image.repository=602401143452.dkr.ecr.${reg}.amazonaws.com/amazon/aws-load-balancer-controller 
-
+        cn=$(echo ${data.aws_eks_cluster.eks_cluster.name})
+        echo "$reg $cn $acc"
+        ./post-policy.sh $reg $cn $acc
      EOT
 }
 }
---
+++ lb2/post-policy.sh
@@ -0,0 +1,8 @@
+test -n "$1" && echo REGION is "$1" || "echo REGION is not set && exit"
+test -n "$2" && echo CLUSTER is "$2" || "echo CLUSTER is not set && exit"
+test -n "$3" && echo ACCOUNT is "$3" || "echo ACCOUNT is not set && exit"
+eksctl utils associate-iam-oidc-provider   --region $1 --cluster $2  --approve
+eksctl create iamserviceaccount --cluster mycluster1 --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::$3:policy/AWSLoadBalancerControllerIAMPolicy --approve
+#helm repo add eks https://aws.github.io/eks-charts
+kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
+helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=$2 --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller --set image.repository=602401143452.dkr.ecr.$1.amazonaws.com/amazon/aws-load-balancer-controller 
