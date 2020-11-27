87b69ef77a5ae86ba0ec269d47ccd65140f9da7d
awsandy <awsandy@amazon.co.uk>
2020-11-27 09:57:22 +0000

    up

--- eks-cidr/null-cidr.tf
+++ eks-cidr/null-cidr.tf
@@ -9,7 +9,12 @@ provisioner "local-exec" {
         echo -e "\x1B[31m Warning! ${data.aws_subnet.i1.availability_zone}\x1B[0m"
         echo -e "\x1B[31m Warning! ${data.aws_subnet.i1.id}\x1B[0m"
         az1=$(echo ${data.aws_subnet.i1.availability_zone})
-        echo $az1
+        az2=$(echo ${data.aws_subnet.i2.availability_zone})
+        az3=$(echo ${data.aws_subnet.i3.availability_zone})
+        sub1=$(echo ${data.aws_subnet.i1.id})
+        sub2=$(echo ${data.aws_subnet.i2.id})
+        sub3=$(echo ${data.aws_subnet.i3.id})
+        echo $az1 $az2 $az3 $sub1 $sub2 $sub3
         echo "************************************************************************************"
      EOT
 }
