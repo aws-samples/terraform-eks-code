4f361245a071621b9fca1f6dc9b803df1a5039ae
awsandy <awsandy@amazon.co.uk>
2020-11-27 16:05:55 +0000

    up

--- lb2/null_post_policy.tf
+++ lb2/null_post_policy.tf
@@ -7,14 +7,18 @@ provisioner "local-exec" {
     on_failure  = fail
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        eksctl utils associate-iam-oidc-provider   --region $AWS_REGION --cluster mycluster1  --approve
+        carn=$(echo ${data.aws_eks_cluster.eks_cluster.arn})
+        reg=$(echo $carn | cut -f4 -d':')
+        acc=$(echo $carn | cut -f5 -d':')
+        echo "$reg $acc"
+        eksctl utils associate-iam-oidc-provider   --region $reg --cluster ${data.aws_eks_cluster.eks_cluster.name}  --approve
         # helm chart does this if you let it
-        eksctl create iamserviceaccount --cluster mycluster1 --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::566972129213:policy/AWSLoadBalancerControllerIAMPolicy --approve
+        eksctl create iamserviceaccount --cluster mycluster1 --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::${acc}:policy/AWSLoadBalancerControllerIAMPolicy --approve
         #helm repo add eks https://aws.github.io/eks-charts
 
         kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
 
-        helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=mycluster1 --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller --set image.repository=602401143452.dkr.ecr.eu-west-1.amazonaws.com/amazon/aws-load-balancer-controller 
+        helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=${data.aws_eks_cluster.eks_cluster.name} --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller --set image.repository=602401143452.dkr.ecr.${reg}.amazonaws.com/amazon/aws-load-balancer-controller 
 
      EOT
 }
