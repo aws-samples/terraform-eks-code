65847b09e1baf9a7142dc4e5cfbe60e9411558ca
awsandy <awsandy@amazon.co.uk>
2020-11-27 15:04:58 +0000

    up

--- lb2/aws_iam_policy-lb2.tf
+++ lb2/aws_iam_policy-lb2.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "load-balancer-policy" {
-  depends_on=[null_policy.policy]
+  depends_on=[null_resource.policy]
   name        = "AWSLoadBalancerControllerIAMPolicy"
   path        = "/"
   description = "AWS LoadBalancer Controller IAM Policy"
