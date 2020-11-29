1d688010b1d8945cf9887cf1c314df02daee5eb0
awsandy <awsandy@amazon.co.uk>
2020-11-29 22:34:52 +0000

    up

--- eks-cidr/null-cidr.tf
+++ eks-cidr/null-cidr.tf
@@ -14,8 +14,8 @@ provisioner "local-exec" {
         sub3=$(echo ${data.aws_subnet.i3.id})
         cn=$(echo ${data.aws_eks_cluster.eks_cluster.name})
         echo $az1 $az2 $az3 $sub1 $sub2 $sub3 $cn
-        #echo -e "\x1B[35mCycle nodes for custom CNI setting (takes a few minutes) ......\x1B[0m"
-        #./cni-cycle-nodes.sh $cn
+        echo -e "\x1B[35mCycle nodes for custom CNI setting (takes a few minutes) ......\x1B[0m"
+        ./cni-cycle-nodes.sh $cn
         echo -e "\x1B[33mAnnotate nodes ......\x1B[0m"
         ./annotate-nodes.sh $az1 $az2 $az3 $sub1 $sub2 $sub3 $cn
         echo -e "\x1B[32mShould see coredns on 100.64.x.y addresses ......\x1B[0m"
--- lb2/post-policy.sh
+++ lb2/post-policy.sh
@@ -6,5 +6,4 @@ eksctl create iamserviceaccount --cluster mycluster1 --namespace=kube-system --n
 #helm repo add eks https://aws.github.io/eks-charts
 kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
 helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=$2 --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller --set image.repository=602401143452.dkr.ecr.$1.amazonaws.com/amazon/aws-load-balancer-controller
-sleep 3
-kubectl get pods -A  | grep aws-load-balancer-controller
+
