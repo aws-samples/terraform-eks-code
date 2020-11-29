51e9e749f74e76bcc7d006615028cf503c013f7c
awsandy <awsandy@amazon.co.uk>
2020-11-29 22:53:16 +0000

    up

--- destroy-everything.sh
+++ destroy-everything.sh
@@ -3,6 +3,8 @@ dirs="cicd lb2 eks-cidr nodeg cluster c9net iam net tf-setup"
 for i in $dirs; do
 cd $i
 echo "Destroying in $i"
+rm -rf .terraform
+terraform init
 terraform destroy -auto-approve
 rm -rf .terraform
 cd $cur
