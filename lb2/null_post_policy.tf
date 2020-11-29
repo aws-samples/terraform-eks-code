bf1a40a3ddd00367dfccc3e253ceb9674e6ea295
awsandy <awsandy@amazon.co.uk>
2020-11-29 22:29:08 +0000

    up

--- lb2/null_post_policy.tf
+++ lb2/null_post_policy.tf
@@ -7,13 +7,13 @@ provisioner "local-exec" {
     on_failure  = fail
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        reg=${data.aws_eks_cluster.eks_cluster.arn}
-        echo $reg
-        reg=$(echo ${data.aws_eks_cluster.eks_cluster.arn} | cut -f4 -d':')
-        acc=$(echo ${data.aws_eks_cluster.eks_cluster.arn} | cut -f5 -d':')
-        cn=$(echo ${data.aws_eks_cluster.eks_cluster.name})
-        echo "$reg $cn $acc"
-        ./post-policy.sh $reg $cn $acc
+        #reg=${data.aws_eks_cluster.eks_cluster.arn}
+        #echo $reg
+        #reg=$(echo ${data.aws_eks_cluster.eks_cluster.arn} | cut -f4 -d':')
+        #acc=$(echo ${data.aws_eks_cluster.eks_cluster.arn} | cut -f5 -d':')
+        #cn=$(echo ${data.aws_eks_cluster.eks_cluster.name})
+        #echo "$reg $cn $acc"
+        ./post-policy.sh eu-west-1 mycluster1 556972129213
      EOT
 }
 }
