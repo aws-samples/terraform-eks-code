bc0a5e031ac344dfb43c26efdd575e2f1161b977
awsandy <awsandy@amazon.co.uk>
2020-11-30 11:13:42 +0000

    up

--- lb2/null_destoy.tf
+++ lb2/null_destoy.tf
@@ -1,10 +1,11 @@
 resource "null_resource" "destroy" {
 depends_on=[aws_iam_policy.load-balancer-policy]
 triggers = {
-    when = "destroy"
+       always_run = "${timestamp()}"
 }
 provisioner "local-exec" {
     on_failure  = fail
+    when    = "destroy"
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
         #reg=$(echo ${data.aws_eks_cluster.eks_cluster.arn} | cut -f4 -d':')
--- lb2/null_post_policy.tf
+++ lb2/null_post_policy.tf
@@ -6,11 +6,12 @@ triggers = {
 provisioner "local-exec" {
     on_failure  = fail
     interpreter = ["/bin/bash", "-c"]
+    when = "create"
     command     = <<EOT
-        #reg=$(echo ${data.aws_eks_cluster.eks_cluster.arn} | cut -f4 -d':')
-        #acc=$(echo ${data.aws_eks_cluster.eks_cluster.arn} | cut -f5 -d':')
-        #cn=$(echo ${data.aws_eks_cluster.eks_cluster.name})
-        #echo "$reg $cn $acc"
+        reg=$(echo ${data.aws_eks_cluster.eks_cluster.arn} | cut -f4 -d':')
+        acc=$(echo ${data.aws_eks_cluster.eks_cluster.arn} | cut -f5 -d':')
+        cn=$(echo ${data.aws_eks_cluster.eks_cluster.name})
+        echo "$reg $cn $acc"
         ./post-policy.sh $reg $cn $acc
         echo "done"
      EOT
