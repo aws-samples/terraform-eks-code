de1acb396898ef6695c14cac56bcc04e5bb751be
awsandy <awsandy@amazon.co.uk>
2020-11-30 09:34:09 +0000

    up

--- cluster/aws_iam_openid_connect_provider.tf
+++ cluster/aws_iam_openid_connect_provider.tf
@@ -3,6 +3,7 @@ data "tls_certificate" "cluster" {
 }
 resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = concat([data.tls_certificate.cluster.certificates.0.sha1_fingerprint], var.oidc_thumbprint_list)
+#  thumbprint_list = concat([data.tls_certificate.cluster.certificates.0.sha1_fingerprint], var.oidc_thumbprint_list)
+  thumbprint_list = [data.tls_certificate.cluster.certificates.0.sha1_fingerprint]
   url = aws_eks_cluster.mycluster1.identity.0.oidc.0.issuer
 }
