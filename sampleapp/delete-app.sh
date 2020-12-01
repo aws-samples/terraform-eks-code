b4367704767618007d951bdcdd448b80fc069143
awsandy <awsandy@amazon.co.uk>
2020-12-01 20:08:15 +0000

    up

--- sampleapp/delete-app.sh
+++ sampleapp/delete-app.sh
@@ -1,4 +1,4 @@
 cat 2048_ingress.yml |  sed 's=alb.ingress.kubernetes.io/target-type: ip=alb.ingress.kubernetes.io/target-type: instance=g' | kubectl delete -f -
-kubectl delete -f 2048-service.yml
-kubectl delete -f 2048-deployment.yml
-kubectl delete -f 2048-namespace.yml
+kubectl delete -f 2048_service.yml
+kubectl delete -f 2048_deployment.yml
+kubectl delete -f 2048_namespace.yml
--- sampleapp/deploy-app.sh
+++ sampleapp/deploy-app.sh
@@ -1,4 +1,4 @@
-kubectl apply -f 2048-namespace.yml
-cat 2048-deployment.yml | ./subvar.sh | kubectl apply -f -
-kubectl apply -f 2048-service.yml
+kubectl apply -f 2048_namespace.yml
+cat 2048_deployment.yml | ./subvar.sh | kubectl apply -f -
+kubectl apply -f 2048_service.yml
 kubectl apply -f 2048_ingress.yml 
