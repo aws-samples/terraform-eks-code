45a1bbed7629eea0c6e1c1d062b0b385faf3e28b
awsandy <awsandy@amazon.co.uk>
2020-12-02 18:12:24 +0000

    up

--- cicd/aws_iam_user.git-user.tf
+++ cicd/aws_iam_user.git-user.tf
@@ -6,3 +6,9 @@ resource "aws_iam_user" "git-user" {
     workshop = "eks-cicd"
   }
 }
+
+
+resource "aws_iam_user_policy_attachment" "git-attach" {
+  user       = aws_iam_user.git-user.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser"
+}
