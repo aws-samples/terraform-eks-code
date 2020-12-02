ea3c8a3f4b8b37bdb2febcdca2fcf30fe1f05e2b
awsandy <awsandy@amazon.co.uk>
2020-12-02 15:24:55 +0000

    up

--- c9net/cicd-route-add.tf
+++ c9net/cicd-route-add.tf
@@ -4,7 +4,7 @@ resource "aws_route" "rt-cicd" {
   vpc_peering_connection_id = aws_vpc_peering_connection.cicd-peer.id
 }
 
-resource "aws_route" "rt-def" {
+resource "aws_route" "rt-def-cicd" {
   route_table_id            = data.aws_vpc.vpc-cicd.main_route_table_id
   destination_cidr_block    = data.terraform_remote_state.net.outputs.eks-cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
