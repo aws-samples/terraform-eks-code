27d41aa9fb2334dc219f80653ee4bc91daed9218
awsandy <awsandy@amazon.co.uk>
2020-12-02 13:17:20 +0000

    up

--- eks-cidr2/annotate-nodes2.sh
+++ eks-cidr2/annotate-nodes2.sh
@@ -8,7 +8,7 @@ sub3=$(echo $6)
 CLUSTER=$(echo $7)
 kubectl get crd
 # get the SG's
-INSTANCE_IDS=(`aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --filters "Name=tag-key,Values=eks:cluster-name" "Name=tag-value,Values=$CLUSTER" "Name=instance-state-name,Values=running" --output text`)
+INSTANCE_IDS=(`aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --filters "Name=tag-key,Values=eks:nodegroup-name" "Name=tag-value,Values=ng2-mycluster1" "Name=instance-state-name,Values=running" --output text`)
 for i in "${INSTANCE_IDS[0]}"
 do
 echo "Descr EC2 instance $i ..."
@@ -20,62 +20,62 @@ echo "subnet $sub2 zone $zone2"
 echo "subnet $sub3 zone $zone3"
 
 echo ${zone1}
-cat << EOF > ${zone1}-pod-netconfig.yaml
+cat << EOF > ${zone1}-pod-netconfig2.yaml
 apiVersion: crd.k8s.amazonaws.com/v1alpha1
 kind: ENIConfig
 metadata:
- name: ${zone1}-pod-netconfig
+ name: ${zone1}-pod-netconfig2
 spec:
  subnet: ${sub1}
  securityGroups:
  - ${sg0}
 EOF
-echo "cat ${zone1}-pod-netconfig.yaml"
-cat ${zone1}-pod-netconfig.yaml
+echo "cat ${zone1}-pod-netconfig2.yaml"
+cat ${zone1}-pod-netconfig2.yaml
 #
 
 echo ${zone2}
-cat << EOF > ${zone2}-pod-netconfig.yaml
+cat << EOF > ${zone2}-pod-netconfig2.yaml
 apiVersion: crd.k8s.amazonaws.com/v1alpha1
 kind: ENIConfig
 metadata:
- name: ${zone2}-pod-netconfig
+ name: ${zone2}-pod-netconfig2
 spec:
  subnet: ${sub2}
  securityGroups:
  - ${sg0}
 EOF
 
-echo "cat ${zone2}-pod-netconfig.yaml"
-cat ${zone2}-pod-netconfig.yaml
+echo "cat ${zone2}-pod-netconfig2.yaml"
+cat ${zone2}-pod-netconfig2.yaml
 #
 echo ${zone3}
-cat << EOF > ${zone3}-pod-netconfig.yaml
+cat << EOF > ${zone3}-pod-netconfig2.yaml
 apiVersion: crd.k8s.amazonaws.com/v1alpha1
 kind: ENIConfig
 metadata:
- name: ${zone3}-pod-netconfig
+ name: ${zone3}-pod-netconfig2
 spec:
  subnet: ${sub3}
  securityGroups:
  - ${sg0}
 EOF
-echo "cat ${zone3}-pod-netconfig.yaml"
-cat ${zone3}-pod-netconfig.yaml
+echo "cat ${zone3}-pod-netconfig2.yaml"
+cat ${zone3}-pod-netconfig2.yaml
 
 echo "apply the CRD ${zone1}"
-kubectl apply -f ${zone1}-pod-netconfig.yaml
+kubectl apply -f ${zone1}-pod-netconfig2.yaml
 echo "apply the CRD ${zone2}"
-kubectl apply -f ${zone2}-pod-netconfig.yaml
+kubectl apply -f ${zone2}-pod-netconfig2.yaml
 echo "apply the CRD ${zone3}"
-kubectl apply -f ${zone3}-pod-netconfig.yaml
-allnodes=`kubectl get nodes -o json`
-len=`kubectl get nodes -o json | jq '.items | length-1'`
+kubectl apply -f ${zone3}-pod-netconfig2.yaml
+allnodes=`kubectl get node --selector='eks.amazonaws.com/nodegroup==ng2-mycluster1' -o json`
+len=`kubectl get node --selector='eks.amazonaws.com/nodegroup==ng2-mycluster1' -o json | jq '.items | length-1'`
 for i in `seq 0 $len`; do
 nn=`echo $allnodes | jq ".items[(${i})].metadata.name" | tr -d '"'`
 nz=`echo $allnodes | jq ".items[(${i})].metadata.labels" | grep failure | grep zone | cut -f2 -d':' | tr -d ' ' | tr -d ','| tr -d '"'`
 echo $nn $nz $nr
-echo "kubectl annotate node ${nn} k8s.amazonaws.com/eniConfig=${nz}-pod-netconfig"
-kubectl annotate node ${nn} k8s.amazonaws.com/eniConfig=${nz}-pod-netconfig
+echo "kubectl annotate node ${nn} k8s.amazonaws.com/eniConfig=${nz}-pod-netconfig2"
+kubectl annotate node ${nn} k8s.amazonaws.com/eniConfig=${nz}-pod-netconfig2
 done
 
--- eks-cidr2/null-cidr.tf
+++ eks-cidr2/null-cidr.tf
@@ -1,17 +1,18 @@
-resource "null_resource" "cidr" {
+resource "null_resource" "cidr2" {
 triggers = {
     always_run = "${timestamp()}"
 }
 provisioner "local-exec" {
     on_failure  = fail
+    when=create
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        az1=$(echo ${data.aws_subnet.i1.availability_zone})
-        az2=$(echo ${data.aws_subnet.i2.availability_zone})
-        az3=$(echo ${data.aws_subnet.i3.availability_zone})
-        sub1=$(echo ${data.aws_subnet.i1.id})
-        sub2=$(echo ${data.aws_subnet.i2.id})
-        sub3=$(echo ${data.aws_subnet.i3.id})
+        az1=$(echo ${data.aws_subnet.p1.availability_zone})
+        az2=$(echo ${data.aws_subnet.p2.availability_zone})
+        az3=$(echo ${data.aws_subnet.p3.availability_zone})
+        sub1=$(echo ${data.aws_subnet.p1.id})
+        sub2=$(echo ${data.aws_subnet.p2.id})
+        sub3=$(echo ${data.aws_subnet.p3.id})
         cn=$(echo ${data.aws_eks_cluster.eks_cluster.name})
         echo $az1 $az2 $az3 $sub1 $sub2 $sub3 $cn
         echo -e "\x1B[35mCycle nodes for custom CNI setting (takes a few minutes) ......\x1B[0m"
