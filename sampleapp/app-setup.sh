6f467ef7e5963d1bb794e98d7c3ecdea37ebc1ee
awsandy <awsandy@amazon.co.uk>
2020-12-02 18:30:19 +0000

    up

--- sampleapp/app-setup.sh
+++ sampleapp/app-setup.sh
@@ -5,26 +5,6 @@ aws iam create-service-specific-credential --user-name git-user --service-name c
 GIT_USERNAME=$(cat /tmp/gituser_output.json | jq -r '.ServiceSpecificCredential.ServiceUserName')
 GIT_PASSWORD=$(cat /tmp/gituser_output.json | jq -r '.ServiceSpecificCredential.ServicePassword')
 CREDENTIAL_ID=$(cat /tmp/gituser_output.json | jq -r '.ServiceSpecificCredential.ServiceSpecificCredentialId')
-test -n "$GIT_USERNAME" && echo GIT_USERNAME is "$GIT_USERNAME" || "echo GIT_USERNAME is not set && exit"
-git clone codecommit::$AWS_REGION://eksworkshop-app
-
-
-#
-
-# auth pipeline
-# aws eks
-
-ROLE="    - rolearn: arn:aws:iam::$ACCOUNT_ID:role/codebuild-eks-cicd-build-app-service-role\n      username: build\n      groups:\n        - system:masters"
-#
-kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapRoles: \|/{print;print \"$ROLE\";next}1" > /tmp/aws-auth-patch.yml
-#
-kubectl patch configmap/aws-auth -n kube-system --patch "$(cat /tmp/aws-auth-patch.yml)"
-#
-
-#eksctl create iamidentitymapping \
-#  --cluster mycluster1 \
-#  --arn arn:aws:iam::${ACCOUNT_ID}:role/codebuild-eks-cicd-build-app-service-role \
-#  --username admin \
-#  --group system:masters
+test -n "$GIT_USERNAME" && echo GIT_USERNAME is "$GIT_USERNAME" || "echo GIT_USERNAME is not set && exit"git clone codecommit::$AWS_REGION://eksworkshop-app
 
 
