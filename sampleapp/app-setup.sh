c8ce2d6b50948038d6bda6fddf0c7112938214f4
awsandy <awsandy@amazon.co.uk>
2020-12-03 22:09:02 +0000

    up

--- sampleapp/app-setup.sh
+++ sampleapp/app-setup.sh
@@ -5,6 +5,7 @@ aws iam create-service-specific-credential --user-name git-user --service-name c
 GIT_USERNAME=$(cat /tmp/gituser_output.json | jq -r '.ServiceSpecificCredential.ServiceUserName')
 GIT_PASSWORD=$(cat /tmp/gituser_output.json | jq -r '.ServiceSpecificCredential.ServicePassword')
 CREDENTIAL_ID=$(cat /tmp/gituser_output.json | jq -r '.ServiceSpecificCredential.ServiceSpecificCredentialId')
-test -n "$GIT_USERNAME" && echo GIT_USERNAME is "$GIT_USERNAME" || "echo GIT_USERNAME is not set && exit"git clone codecommit::$AWS_REGION://eksworkshop-app
+test -n "$GIT_USERNAME" && echo GIT_USERNAME is "$GIT_USERNAME" || "echo GIT_USERNAME is not set && exit"
+git clone codecommit::$AWS_REGION://eksworkshop-app
 
 
