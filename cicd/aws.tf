ac8e09fecc3875267f99f66725ed08fece5b9a1d
awsandy <awsandy@amazon.co.uk>
2020-12-05 16:24:01 +0000

    up

--- cicd/aws.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region                  = "eu-west-1"
-  shared_credentials_file = "~/.aws/credentials"
-  version                 = "= 3.8.0"
-  profile                 = "default"
-}
