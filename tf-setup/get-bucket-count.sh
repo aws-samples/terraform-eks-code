a14936f0cb354f7ecc9330b3e8c1bc2dcc1b3402
awsandy <awsandy@amazon.co.uk>
2020-12-06 15:17:22 +0000

    up

--- tf-setup/get-bucket-count.sh
+++ tf-setup/get-bucket-count.sh
@@ -3,7 +3,7 @@
 set -e
 BUCKET_COUNT=0
 res=`terraform state list aws_s3_bucket.terraform_state || true`
-if [[ $? -eq 1 ]]; then 
+if [[ "$res" == "" ]]; then
 BUCKET_COUNT=1
 fi
 jq -n --arg bc "$BUCKET_COUNT" '{"Count":$bc}'
