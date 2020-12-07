f8e7a9e6ef1801d249952271fc3f72eba3edfcfb
awsandy <awsandy@amazon.co.uk>
2020-12-07 18:09:13 +0000

    up

--- cluster/aws_iam_openid_connect_provider.tf
+++ cluster/aws_iam_openid_connect_provider.tf
@@ -1,3 +1,4 @@
+## OIDC Provider
 data "tls_certificate" "cluster" {
   url = aws_eks_cluster.mycluster1.identity.0.oidc.0.issuer
 }
@@ -6,4 +7,29 @@ resource "aws_iam_openid_connect_provider" "cluster" {
 #  thumbprint_list = concat([data.tls_certificate.cluster.certificates.0.sha1_fingerprint], var.oidc_thumbprint_list)
   thumbprint_list = [data.tls_certificate.cluster.certificates.0.sha1_fingerprint]
   url = aws_eks_cluster.mycluster1.identity.0.oidc.0.issuer
+}
+
+
+### 
+data "aws_iam_policy_document" "cluster_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.cluster.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.cluster.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "cluster" {
+  assume_role_policy = data.aws_iam_policy_document.cluster_assume_role_policy.json
+  name               = "cluster"
 }
