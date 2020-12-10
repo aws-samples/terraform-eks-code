0bb3077b2b4f92f38ac68423a9f52a36483feec9
awsandy <awsandy@amazon.co.uk>
2020-12-10 15:54:00 +0000

    up

--- cluster/null_resource.tf
+++ cluster/null_resource.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "gen_backend" {
+resource "null_resource" "gen_cluster_auth" {
 triggers = {
     always_run = "${timestamp()}"
 }
@@ -12,8 +12,6 @@ provisioner "local-exec" {
         ./test.sh ${aws_eks_cluster.mycluster1.name}
         echo -e "\x1B[32m Checking Authorization ${aws_eks_cluster.mycluster1.name}...should see Server Version: v1.17.xxx \x1B[0m"
         ./auth.sh ${aws_eks_cluster.mycluster1.name}
-        #kubectl set env ds aws-node -n kube-system AWS_VPC_K8S_CNI_CUSTOM_NETWORK_CFG=true
-        #kubectl describe daemonset aws-node -n kube-system | grep -A5 Environment | grep CUSTOM
         echo "************************************************************************************"
      EOT
 }
