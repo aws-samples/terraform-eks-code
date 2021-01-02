c6e8810d8fe5d9a28bf4de1c78ae08335dc7e2a8
awsandy <awsandy@amazon.co.uk>
2021-01-02 17:05:21 +0000

    up

--- tf-setup/aws.tf
+++ tf-setup/aws.tf
@@ -1,11 +1,15 @@
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  required_version = ">= 0.14"
+  required_providers {
+    aws {
+    source = "hashicord/aws"
+    #  Allow any 3.1x version of the AWS provider
+    version = "~> 3.22"
+  }
 }
 
 provider "aws" {
   region                  = var.region
   shared_credentials_file = "~/.aws/credentials"
   profile                 = var.profile
-  # Allow any 3.1x version of the AWS provider
-  version = "~> 3.10"
 }
