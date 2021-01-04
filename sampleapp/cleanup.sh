cd4b465ef35a8d5e8ef95c770747f76d0b15fc1c
awsandy <awsandy@amazon.co.uk>
2021-01-04 11:15:47 +0000

    up

--- sampleapp/cleanup.sh
+++ sampleapp/cleanup.sh
@@ -1,6 +1,8 @@
-#cat 2048_ingress.yml |  sed 's=alb.ingress.kubernetes.io/target-type: ip=alb.ingress.kubernetes.io/target-type: instance=g' | kubectl delete -f -
+set +e
+cat 2048_ingress.yml |  sed 's=alb.ingress.kubernetes.io/target-type: ip=alb.ingress.kubernetes.io/target-type: instance=g' | kubectl delete -f -
 kubectl delete -f 2048_service.yml
 kubectl delete -f 2048_deployment.yml
 kubectl delete -f 2048_namespace.yml
 userid=$(aws iam list-service-specific-credentials --user-name git-user | jq -r .ServiceSpecificCredentials[0].ServiceSpecificCredentialId)
 aws iam delete-service-specific-credential --service-specific-credential-id $userid --user-name git-user
+set -e
