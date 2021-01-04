ee2ee08834823298ddd32d328442a83af84030c4
awsandy <awsandy@amazon.co.uk>
2021-01-04 09:07:08 +0000

    up

--- sampleapp/cleanup.sh
+++ sampleapp/cleanup.sh
@@ -2,5 +2,5 @@
 kubectl delete -f 2048_service.yml
 kubectl delete -f 2048_deployment.yml
 kubectl delete -f 2048_namespace.yml
-userid=$(aws iam list-service-specific-credentials --user-name git-user | jq -r .ServiceSpecificCredentials[0].ServiceSpecificCredentialId )
-aws iam delete-service-specific-credential --service-specific-credential-id $userid
+userid=$(aws iam list-service-specific-credentials --user-name git-user | jq -r .ServiceSpecificCredentials[0].ServiceSpecificCredentialId)
+aws iam delete-service-specific-credential --service-specific-credential-id $userid --user-name git-user
