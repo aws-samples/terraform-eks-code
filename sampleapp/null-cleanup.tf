048772959f6bdee73ea3553776dc45e7438dd446
awsandy <awsandy@amazon.co.uk>
2021-01-04 09:31:03 +0000

    cleanup

--- sampleapp/null-cleanup.tf
+++ sampleapp/null-cleanup.tf
@@ -2,7 +2,7 @@ resource "null_resource" "cleanup" {
 triggers = {
     always_run = timestamp()
 }
-depends_on     = [aws_iam_role.codebuild-eks-cicd-build-app-service-role]
+
 provisioner "local-exec" {
     on_failure  = fail
     when = destroy
