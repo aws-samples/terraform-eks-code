fd85419185750d7386c45dc3f0a5d682ce3e5fe2
awsandy <awsandy@amazon.co.uk>
2021-01-04 09:46:35 +0000

    up

--- sampleapp/app-setup.sh
+++ sampleapp/app-setup.sh
@@ -2,9 +2,10 @@
 test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || "echo AWS_REGION is not set && exit"
 #aws iam attach-user-policy --user-name git-user --policy-arn arn:aws:iam::aws:policy/AWSCodeCommitPowerUser
 aws iam create-service-specific-credential --user-name git-user --service-name codecommit.amazonaws.com | tee /tmp/gituser_output.json
-GIT_USERNAME=$(cat /tmp/gituser_output.json | jq -r '.ServiceSpecificCredential.ServiceUserName')
-GIT_PASSWORD=$(cat /tmp/gituser_output.json | jq -r '.ServiceSpecificCredential.ServicePassword')
-CREDENTIAL_ID=$(cat /tmp/gituser_output.json | jq -r '.ServiceSpecificCredential.ServiceSpecificCredentialId')
+usercred=$(aws iam create-service-specific-credential --user-name git-user --service-name codecommit.amazonaws.com)
+GIT_USERNAME=$(echo $usercred | jq -r '.ServiceSpecificCredential.ServiceUserName')
+GIT_PASSWORD=$(echo $usercred | jq -r '.ServiceSpecificCredential.ServicePassword')
+CREDENTIAL_ID=$(echo $usercred | jq -r '.ServiceSpecificCredential.ServiceSpecificCredentialId')
 test -n "$GIT_USERNAME" && echo GIT_USERNAME is "$GIT_USERNAME" || "echo GIT_USERNAME is not set && exit"
 git clone codecommit::$AWS_REGION://eksworkshop-app
 
