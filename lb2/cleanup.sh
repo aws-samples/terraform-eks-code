08641513cade7a24778f298685ab1e46dd4cdfbd
awsandy <awsandy@amazon.co.uk>
2021-01-04 10:50:10 +0000

    up

--- lb2/cleanup.sh
+++ lb2/cleanup.sh
@@ -4,4 +4,4 @@ echo "Remove helm deployment"
 helm delete aws-load-balancer-controller -n kube-system
 echo "Remove CRD"
 kubectl delete -f crds.yaml
-
+#aws iam delete-policy --policy-arnarn:aws:iam::$AWS_ACCOUNT:policy/AWSLoadBalancerControllerIAMPolicy
---
+++ sampleapp/deploy-ingress.sh
@@ -0,0 +1,1 @@
+kubectl apply -f 2048_ingress.yml 
