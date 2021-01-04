6cdd044495a14fbaa64990f0c5d478cc97bda1e2
awsandy <awsandy@amazon.co.uk>
2021-01-04 09:02:02 +0000

    cleanup

---
+++ sampleapp/null-cleanup.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "auth-cidr" {
+triggers = {
+    always_run = timestamp()
+}
+depends_on     = [aws_iam_role.codebuild-eks-cicd-build-app-service-role]
+provisioner "local-exec" {
+    on_failure  = fail
+    when = destroy
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOT
+        echo "remote git credentials &" sample app
+        ./cleanup.sh
+        echo "************************************************************************************"
+     EOT
+}
+}
