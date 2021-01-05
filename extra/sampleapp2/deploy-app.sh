00edbd72d5f9b862872a70d0b76c47b51edfd003
awsandy <awsandy@amazon.co.uk>
2021-01-05 16:10:45 +0000

    up

--- extra/sampleapp2/deploy-app.sh
+++ extra/sampleapp2/deploy-app.sh
@@ -1,5 +1,6 @@
 kubectl apply -f 2048_namespace.yml
 cat 2048_deployment-ng1.yml | ./subvar.sh | kubectl apply -f -
 cat 2048_deployment-ng2.yml | ./subvar.sh | kubectl apply -f -
-kubectl apply -f 2048_service.yml
+kubectl apply -f 2048_service1.yml
+kubectl apply -f 2048_service2.yml
 kubectl apply -f 2048_ingress.yml 
--- sampleapp/delete-app.sh
+++ sampleapp/delete-app.sh
@@ -1,4 +1,5 @@
 set +e
+echo "Deleting an Ingress takes a few minutes......"
 cat 2048_ingress.yml |  sed 's=alb.ingress.kubernetes.io/target-type: ip=alb.ingress.kubernetes.io/target-type: instance=g' | kubectl delete -f -
 kubectl delete -f 2048_service.yml
 kubectl delete -f 2048_deployment.yml
