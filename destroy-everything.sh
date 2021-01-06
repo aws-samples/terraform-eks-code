723615f1e79e3dee0d9ed38e0156d8e4530e690c
awsandy <awsandy@amazon.co.uk>
2021-01-06 16:02:19 +0000

    up

--- destroy-everything.sh
+++ destroy-everything.sh
@@ -1,5 +1,5 @@
 cur=`pwd`
-dirs="extra/eks-cidr2 extra/nodeg2 sample-app lb2 eks-cidr cicd nodeg cluster c9net iam net tf-setup"
+dirs="extra/eks-cidr2 extra/nodeg2 sampleapp lb2 eks-cidr cicd nodeg cluster c9net iam net tf-setup"
 for i in $dirs; do
 cd $i
 echo "Destroying in $i"
