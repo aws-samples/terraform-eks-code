7a851c28f7482924a709cc535aeb4f23cc799353
awsandy <awsandy@amazon.co.uk>
2021-01-08 17:29:12 +0000

    up

--- extra/sampleapp2/deploy-app.sh
+++ extra/sampleapp2/deploy-app.sh
@@ -1,6 +1,7 @@
-kubectl apply -f 2048_namespace.yml
+kubectl apply -f 2048_namespace1.yml
+kubectl apply -f 2048_namespace2.yml
 cat 2048_deployment-ng1.yml | ./subvar.sh | kubectl apply -f -
 cat 2048_deployment-ng2.yml | ./subvar.sh | kubectl apply -f -
 kubectl apply -f 2048_service1.yml
 kubectl apply -f 2048_service2.yml
-kubectl apply -f 2048_ingress.yml 
+ 
