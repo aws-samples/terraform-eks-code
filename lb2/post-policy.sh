98bd5e44e3979561b078073d280a8cc43c3275a6
awsandy <awsandy@amazon.co.uk>
2021-01-08 13:57:34 +0000

    up

--- lb2/post-policy.sh
+++ lb2/post-policy.sh
@@ -1,7 +1,7 @@
 test -n "$1" && echo REGION is "$1" || "echo REGION is not set && exit"
 test -n "$2" && echo CLUSTER is "$2" || "echo CLUSTER is not set && exit"
 test -n "$3" && echo ACCOUNT is "$3" || "echo ACCOUNT is not set && exit"
-test -n "$LBC_VERSION" && echo LBC_VERSION is "$LBC_VERSION" || "export LBC_VERSION=2.1.0"
+test -n "$LBC_VERSION" && echo LBC_VERSION is "$LBC_VERSION" || export LBC_VERSION=2.1.0
 #eksctl utils associate-iam-oidc-provider   --region $1 --cluster $2  --approve
 #eksctl create iamserviceaccount --cluster mycluster1 --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::$3:policy/AWSLoadBalancerControllerIAMPolicy --approve
 #helm repo add eks https://aws.github.io/eks-charts
