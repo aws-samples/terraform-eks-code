1ffcc27b71e8baf95a54ff992b13ce394e5bd628
awsandy <awsandy@amazon.co.uk>
2021-01-08 11:10:37 +0000

    up

--- tf-setup/get-bucket-name.sh
+++ tf-setup/get-bucket-name.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Exit if any of the intermediate steps fail
 set -e
-#t2=`date +%s%N`
+t2=`date +%s%N`
 t1=`hostname | cut -f1 -d'.'`
-#BUCKET_NAME=`printf "terraform-state-%s-%s" $t1 $t2 | awk '{print tolower($0)}'`
-BUCKET_NAME=`printf "terraform-state-%s" $t1 | awk '{print tolower($0)}'`
+BUCKET_NAME=`printf "tf-state-%s-%s" $t1 $t2 | awk '{print tolower($0)}'`
 jq -n --arg bn "$BUCKET_NAME" '{"Name":$bn}'
