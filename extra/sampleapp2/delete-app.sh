37a9b77477f3dd2dfb7aec955810cebaf4f3a30e
awsandy <awsandy@amazon.co.uk>
2021-01-08 20:57:45 +0000

    up

--- extra/sampleapp2/delete-app.sh
+++ extra/sampleapp2/delete-app.sh
@@ -1,6 +1,8 @@
-cat 2048_ingress.yml |  sed 's=alb.ingress.kubernetes.io/target-type: ip=alb.ingress.kubernetes.io/target-type: instance=g' | kubectl delete -f -
+cat 2048_ingress1.yml |  sed 's=alb.ingress.kubernetes.io/target-type: ip=alb.ingress.kubernetes.io/target-type: instance=g' | kubectl delete -f -
+cat 2048_ingress2.yml |  sed 's=alb.ingress.kubernetes.io/target-type: ip=alb.ingress.kubernetes.io/target-type: instance=g' | kubectl delete -f -
 kubectl delete -f 2048_service1.yml
 kubectl delete -f 2048_service2.yml
 kubectl delete -f 2048_deployment-ng1.yml
 kubectl delete -f 2048_deployment-ng2.yml
-kubectl delete -f 2048_namespace.yml
+#kubectl delete -f 2048_namespace1.yml
+#kubectl delete -f 2048_namespace2.yml
