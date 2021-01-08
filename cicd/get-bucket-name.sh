552205263be54cc1adc4fe2ebab1b3f0022210da
awsandy <awsandy@amazon.co.uk>
2021-01-08 11:12:22 +0000

    up

--- cicd/get-bucket-name.sh
+++ cicd/get-bucket-name.sh
@@ -4,5 +4,4 @@ set -e
 t2=`date +%s%N`
 t1=`hostname | cut -f1 -d'.'`
 BUCKET_NAME=`printf "codep-tfeks-%s-%s" $t1 $t2 | awk '{print tolower($0)}'`
-$0)}'`
 jq -n --arg bn "$BUCKET_NAME" '{"Name":$bn}'
