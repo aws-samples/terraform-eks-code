850fd00205b15f314d8228cec2404b7732db9432
awsandy <awsandy@amazon.co.uk>
2021-01-10 14:47:03 +0000

    up

---
+++ sampleapp/sampleapp.tf
@@ -0,0 +1,45 @@
+resource "kubernetes_namespace" "game-2048" {
+    name = "game-2048"
+  }
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+resource "kubernetes_deployment" "deployment-2048" {
+  metadata {
+    name = "deployment-2048"
+    namespace="game-2048"
+  }
+
+  spec {
+    replicas = 4
+
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name" = "app-2048"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          "app.kubernetes.io/name" = "app-2048"
+        }
+      }
+
+      spec {
+        container {
+          image = format("%s.dkr.ecr.%s.amazonaws.com/sample-app",data.aws_caller_identity.current.account_id,data.aws_region.current.name)
+          name  = "app-2048"
+          image_pull_policy = "Always"
+          port = ["80"]
+        }
+        node_selector {
+          "alpha.eksctl.io/nodegroup-name" =  "ng1-mycluster1"
+        }
+
+      }
+    }
+  }
+}
