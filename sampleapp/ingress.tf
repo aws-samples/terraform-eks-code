d1bd53f441acedea08e7be60aa1b4bbfe4dea733
awsandy <awsandy@amazon.co.uk>
2021-01-10 18:36:35 +0000

    up

--- sampleapp/ingress.tf
+++ sampleapp/ingress.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_ingress" "game-2048__ingress-2048" {
   metadata {
-    annotations = {"kubernetes.io/ingress.class" = "alb", "alb.ingress.kubernetes.io/scheme" = "internal", "alb.ingress.kubernetes.io/target-type" = "ip", "alb.ingress.kubernetes.io/listen-ports" = "[{'HTTP': 8080}]" }
+    annotations = {"kubernetes.io/ingress.class" = "alb", "alb.ingress.kubernetes.io/scheme" = "internal", "alb.ingress.kubernetes.io/target-type" = "ip", "alb.ingress.kubernetes.io/listen-ports" = "[{\"HTTP\": 8080}]" }
     name        = "ingress-2048"
     namespace   = "game-2048"
   }
