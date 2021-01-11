81f003405c339f011460bca0d73bf1ffa14e989d
awsandy <awsandy@amazon.co.uk>
2021-01-11 11:47:35 +0000

    up

--- extra/sampleapp2/sampleapp-namespace.tf
+++ extra/sampleapp2/sampleapp-namespace.tf
@@ -8,7 +8,7 @@ resource "kubernetes_namespace" "game1-2048" {
   }
 }
 
-esource "kubernetes_namespace" "game2-2048" {
+resource "kubernetes_namespace" "game2-2048" {
   metadata {
     name = "game2-2048"
   }
