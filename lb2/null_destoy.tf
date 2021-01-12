d5870ad5628c01ee78684802535ebb05a63a330d
awsandy <awsandy@amazon.co.uk>
2021-01-12 22:42:59 +0000

    up

--- lb2/null_destoy.tf
+++ lb2/null_destoy.tf
@@ -10,9 +10,11 @@ provisioner "local-exec" {
     command     = <<EOT
         #echo "Remove helm deployment"
         #helm delete aws-load-balancer-controller -n kube-system
+        if [ -f "crds.yml" ]; then
         echo "Remove CRD"
         kubectl delete -f crds.yaml 
         echo "done"
+        fi
      EOT
 }
 }
