db372ecff47e31adc5855fd9d3726f4a20219c1e
awsandy <awsandy@amazon.co.uk>
2021-01-12 19:33:15 +0000

    up

--- testing/build-everything.sh
+++ testing/build-everything.sh
@@ -2,7 +2,8 @@ cur=`pwd`
 dirs="tf-setup net iam c9net cluster nodeg cicd eks-cidr lb2 sampleapp"
 for i in $dirs; do
 cd ../$i
-echo "Building in $i"
+echo " "
+echo "**** Building in $i ****"
 rm -rf .terraform
 terraform init -no-color
 terraform plan -out tfplan -no-color
--- testing/destroy-everything.sh
+++ testing/destroy-everything.sh
@@ -2,7 +2,7 @@ cur=`pwd`
 dirs="sampleapp cicd nodeg cluster c9net iam net tf-setup"
 for i in $dirs; do
 cd ../$i
-echo "Destroying in $i"
+echo "**** Destroying in $i ****"
 terraform destroy -auto-approve
 rm -rf .terraform
 cd $cur
