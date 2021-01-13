202df8629f831f4ec26fc0892957a5b6f46d4ec4
awsandy <awsandy@amazon.co.uk>
2021-01-13 08:12:06 +0000

    up

--- testing/destroy-everything.sh
+++ testing/destroy-everything.sh
@@ -1,6 +1,22 @@
+echo "Pre cli based actions ..."
+userid=$(aws iam list-service-specific-credentials --user-name git-user | jq -r .ServiceSpecificCredentials[0].ServiceSpecificCredentialId)
+if [ "$userid" != "null" ]; then
+aws iam delete-service-specific-credential --service-specific-credential-id $userid --user-name git-user
+fi
+# lb, lb sg, launch template
+echo "pass 1 ...."
 cur=`pwd`
 date
 dirs="sampleapp cicd nodeg cluster c9net iam net tf-setup"
+for i in $dirs; do
+cd ../$i
+echo "**** Destroying in $i ****"
+terraform destroy -auto-approve
+cd $cur
+date
+done
+echo "Pass 1 cli based actions ..."
+echo "pass 2 ...."
 for i in $dirs; do
 cd ../$i
 echo "**** Destroying in $i ****"
@@ -10,4 +26,6 @@ rm -f terraform.tfstate* tfplan
 cd $cur
 date
 done
+echo "Pass 2 cli based actions ..."
+echo "Done"
 exit
