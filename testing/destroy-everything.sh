7b52ca8a9d3319ad894d27730624465a15152c0a
awsandy <awsandy@amazon.co.uk>
2021-01-13 16:04:57 +0000

    up

--- testing/destroy-everything.sh
+++ testing/destroy-everything.sh
@@ -1,11 +1,13 @@
 echo "Pre cli based actions ..."
 userid=$(aws iam list-service-specific-credentials --user-name git-user | jq -r .ServiceSpecificCredentials[0].ServiceSpecificCredentialId)
 if [ "$userid" != "null" ]; then
+echo "destroying git user credentaisl for $userid"
 aws iam delete-service-specific-credential --service-specific-credential-id $userid --user-name git-user
 fi
 # Empty codepipeline bucket ready for delete
 buck=$(aws s3 ls | grep codep-tfeks | awk '{print $3}')
 if [ "$buck" != "" ]; then
+echo "Emptying bucket $buck"
 aws s3 rm s3://$buck --recursive
 fi
 #
