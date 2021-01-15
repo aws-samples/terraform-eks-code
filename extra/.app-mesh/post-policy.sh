e7f398971501caa79a92c4e23ce7064dc0329d7d
awsandy <awsandy@amazon.co.uk>
2021-01-15 19:10:56 +0000

    up

--- extra/.app-mesh/post-policy.sh
+++ extra/.app-mesh/post-policy.sh
@@ -6,7 +6,7 @@ test -n "$LBC_VERSION" && echo LBC_VERSION is "$LBC_VERSION" || export LBC_VERSI
 #eksctl create iamserviceaccount --cluster mycluster1 --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::$3:policy/AWSLoadBalancerControllerIAMPolicy --approve
 #helm repo add eks https://aws.github.io/eks-charts
 rm -f crds.yaml*
-curl -o crds.yml https://raw.githubusercontent.com/aws/eks-charts/master/stable/appmesh-controller/crds/crds.yaml
+curl -o crds.yaml https://raw.githubusercontent.com/aws/eks-charts/master/stable/appmesh-controller/crds/crds.yaml
 kubectl apply -f crds.yaml
 #kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
 #helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=$2 --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller --set image.repository=602401143452.dkr.ecr.$1.amazonaws.com/amazon/aws-load-balancer-controller
