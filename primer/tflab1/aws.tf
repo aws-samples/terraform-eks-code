49a3d4574745224339e0839a34bf8aa7f3dc3eff
awsandy <awsandy@amazon.co.uk>
2021-05-13 18:27:38 +0100

    up

--- primer/tflab1/aws.tf
+++ primer/tflab1/aws.tf
@@ -5,7 +5,7 @@ terraform {
     aws = {
     source = "hashicorp/aws"
     #  Allow any 3.22+  version of the AWS provider
-    version = "~> 3.22"
+    #version = "~> 3.22"
     version = "= 3.39"
     }
   }
