f29b9f43736b472cd439536fa645969384213ca7
awsandy <awsandy@amazon.co.uk>
2021-08-02 20:15:34 +0100

    up

--- .automation/build-everything.sh
+++ .automation/build-everything.sh
@@ -41,7 +41,7 @@ for i in $dirs; do
 done
 
 echo "Some post build verifications"
-echo "Should have 23 pods running in total"
+echo "Should have at least 23 pods running in total"
 rc=$(kubectl get pods -A | grep Running | wc -l)
 if [ $rc -lt 23 ]; then 
 echo "ERROR: Found only $rc pods running - expected 23"
