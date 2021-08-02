ac26606f7f4bcfc26cb39d8c732d2fdbb110472f
awsandy <awsandy@amazon.co.uk>
2021-08-02 19:10:46 +0100

    up

--- net/aws_route_table_association__public1.tf
+++ net/aws_route_table_association__public1.tf
@@ -2,5 +2,5 @@
 # aws_route_table_association.rtbassoc-024cb38ce64e85194:
 resource "aws_route_table_association" "rtbassoc-024cb38ce64e85194" {
   route_table_id = aws_route_table.rtb-0c9df3106b9e1bca9.id
-  subnet_id      = aws_subnet.subnet-05937b8ca518f111e.id
+  subnet_id      = aws_subnet.subnet-cicd-public1.id
 }
