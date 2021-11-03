d32c505c0af0c8b827fc857e0fdf0edbaa2e3a51
awsandy <awsandy@amazon.co.uk>
2021-11-03 17:46:29 +0000

    added AZ vars to setup

--- setup-tools.sh
+++ setup-tools.sh
@@ -22,11 +22,14 @@ if [ $? -eq 0 ]; then
   rm -vf ${HOME}/.aws/credentials
   export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
   export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+  export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
+  export TF_VAR_region=${AWS_REGION}
   test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo "AWS_REGION is not set !!"
   echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
   echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
-  export TF_VAR_region=${AWS_REGION}
+  echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
   echo "export TF_VAR_region=${AWS_REGION}" | tee -a ~/.bash_profile
+  
   aws configure set default.region ${AWS_REGION}
   aws configure get region
 else
