160f15531ce911b7fb78236b2affaad5f38089c4
awsandy <awsandy@amazon.co.uk>
2021-12-09 17:13:34 +0000

    c9 auth

---
+++ cluster/c9-auth.sh
@@ -0,0 +1,25 @@
+test -n "$C9_PID" && echo C9_PID is "$C9_PID" || "echo C9_POID is not set && exit"
+echo "local auth"
+c9builder=$(aws cloud9 describe-environment-memberships --environment-id=$C9_PID | jq -r '.memberships[].userArn')
+if echo ${c9builder} | grep -q user; then
+	rolearn=${c9builder}
+        echo Role ARN: ${rolearn}
+elif echo ${c9builder} | grep -q assumed-role; then
+        assumedrolename=$(echo ${c9builder} | awk -F/ '{print $(NF-1)}')
+        rolearn=$(aws iam get-role --role-name ${assumedrolename} --query Role.Arn --output text) 
+        echo Role ARN: ${rolearn}
+fi
+## need to Terraform this ?
+cat << EOF > patch.yaml
+data:
+  mapUsers: |
+    - userarn: ${rolearn}  
+      username: admin
+      groups:
+        - system:masters
+EOF
+kubectl get configmap -n kube-system aws-auth -o yaml > aws-auth.yaml
+cat patch.yaml >> aws-auth.yaml
+kubectl apply -f aws-auth.yaml
+#eksctl create iamidentitymapping --cluster mycluster1 --arn ${rolearn} --group system:masters --username admin
+kubectl describe configmap -n kube-system aws-auth
