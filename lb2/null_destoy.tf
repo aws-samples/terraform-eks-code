16c8e5d6fda5c8b9c29d9edf06649794f2f5298b
awsandy <awsandy@amazon.co.uk>
2021-12-09 16:54:48 +0000

    set context in destroy

--- lb2/null_destoy.tf
+++ lb2/null_destoy.tf
@@ -12,7 +12,9 @@ provisioner "local-exec" {
         #helm delete aws-load-balancer-controller -n kube-system
         if [ -f "crds.yml" ]; then
         echo "Set context"
-        kubectx ${data.aws_eks_cluster.eks_cluster.arn}
+        arn=$(terraform state show data.aws_eks_cluster.eks_cluster | grep arn | grep :cluster | cut -f2 -d'=' | jq -r .)
+        echo $arn
+        kubectx $arn
         echo "Remove CRD"
         kubectl delete -f crds.yaml 
         echo "done"
