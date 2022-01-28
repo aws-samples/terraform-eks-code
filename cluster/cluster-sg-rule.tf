8e09c9d20b31532f9a3c0290a3f1b11ba970c9db
awsandy <awsandy@amazon.co.uk>
2022-01-28 17:44:01 +0000

    cluster sg rule

---
+++ cluster/cluster-sg-rule.tf
@@ -0,0 +1,8 @@
+resource "aws_security_group_rule" "eks-add-clustersg" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
+  security_group_id        = data.terraform_remote_state.net.outputs.cluster-sg
+}
