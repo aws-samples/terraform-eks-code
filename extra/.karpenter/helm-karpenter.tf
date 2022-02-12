735c540b80b0f17c5112d8b77b5c2374fa41955f
awsandy <awsandy@amazon.co.uk>
2022-02-12 17:33:03 +0000

    up

--- extra/.karpenter/helm-karpenter.tf
+++ extra/.karpenter/helm-karpenter.tf
@@ -57,7 +57,11 @@ resource "local_file" "karpenter_provisioner" {
         # As of today the Provisioner CRD requires it regardless. Fix is coming in future releases
         "instanceProfile" = aws_iam_instance_profile.karpenter_node.name
         "launchTemplate"  = aws_launch_template.bottlerocket.name
-        "karpenter.sh/securityGroupSelector" = data.aws_eks_cluster.eks.vpc_config.0.cluster_security_group_id
+        "karpenter.sh/securityGroupSelector" = {
+          "key"      = "Name"
+          "operator" = "In"
+          "values"   = "${data.aws_eks_cluster.eks.vpc_config.0.cluster_security_group_id}"
+        }        
         "karpenter.sh/subnetSelector" = {
           "key"      = "Name"
           "operator" = "In"
