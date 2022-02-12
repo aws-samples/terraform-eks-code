85ffa913077b45f37c3b257d88a91f03346386fc
awsandy <awsandy@amazon.co.uk>
2022-02-12 16:22:24 +0000

    up

---
+++ extra/.karpenter-provisioner/workload.sh
@@ -0,0 +1,28 @@
+cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: inflate
+spec:
+  replicas: 0
+  selector:
+    matchLabels:
+      app: inflate
+  template:
+    metadata:
+      labels:
+        app: inflate
+    spec:
+      terminationGracePeriodSeconds: 0
+      containers:
+        - name: inflate
+          image: public.ecr.aws/eks-distro/kubernetes/pause:3.2
+          resources:
+            requests:
+              cpu: 1
+EOF
+kubectl scale deployment inflate --replicas 5
+for i in `kubectl get pods -n karpenter -l karpenter=controller -o name`; do
+kubectl logs -n karpenter $i
+done
+#kubectl logs -f -n karpenter $(kubectl get pods -n karpenter -l karpenter=controller -o name | head -1)
