dcae71da33166dc5009b9c86dce5c95e89fd290f
awsandy <awsandy@amazon.co.uk>
2022-02-21 15:19:07 +0000

    rtb isol

--- c9net/eks-route-add.tf
+++ c9net/eks-route-add.tf
@@ -20,6 +20,7 @@ resource "aws_route" "rt-eks-isol" {
   route_table_id            = data.terraform_remote_state.net.outputs.rtb-isol
   destination_cidr_block    = data.aws_vpc.vpc-default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
+}
 
 
 resource "aws_route" "rt-eks4" {
