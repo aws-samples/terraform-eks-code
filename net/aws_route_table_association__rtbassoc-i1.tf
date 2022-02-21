c889fb731d1c8ae097005665f715236b9a34e84b
awsandy <awsandy@amazon.co.uk>
2022-02-21 14:58:16 +0000

    rtb isol op

--- net/aws_route_table_association__rtbassoc-i1.tf
+++ net/aws_route_table_association__rtbassoc-i1.tf
@@ -4,3 +4,6 @@ resource "aws_route_table_association" "rtbassoc-0b44c68881df61a18" {
   route_table_id = aws_route_table.rtb-0329e787bbafcb2c4.id
   subnet_id      = aws_subnet.subnet-i1.id
 }
+output "rtb-isol" {
+  value = aws_route_table.rtb-0329e787bbafcb2c4.id
+}
