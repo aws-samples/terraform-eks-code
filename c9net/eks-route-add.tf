c9ea74afe434457e3ad12eabbc741356a6af29bc
awsandy <awsandy@amazon.co.uk>
2022-02-21 15:18:21 +0000

    up

--- c9net/eks-route-add.tf
+++ c9net/eks-route-add.tf
@@ -16,6 +16,12 @@ resource "aws_route" "rt-eks3" {
   vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
 }
 
+resource "aws_route" "rt-eks-isol" {
+  route_table_id            = data.terraform_remote_state.net.outputs.rtb-isol
+  destination_cidr_block    = data.aws_vpc.vpc-default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
+
+
 resource "aws_route" "rt-eks4" {
   route_table_id            = data.terraform_remote_state.net.outputs.rtb-priv1
   destination_cidr_block    = data.aws_vpc.vpc-cicd.cidr_block
@@ -30,6 +36,12 @@ resource "aws_route" "rt-eks5" {
 
 resource "aws_route" "rt-eks6" {
   route_table_id            = data.terraform_remote_state.net.outputs.rtb-priv3
+  destination_cidr_block    = data.aws_vpc.vpc-cicd.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.cicd-peer.id
+}
+
+resource "aws_route" "rt-cicd-isol" {
+  route_table_id            = data.terraform_remote_state.net.outputs.rtb-isol
   destination_cidr_block    = data.aws_vpc.vpc-cicd.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.cicd-peer.id
 }
