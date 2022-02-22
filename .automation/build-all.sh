361b2e4502695209ab0b971f227173ea26c78735
awsandy <awsandy@amazon.co.uk>
2022-02-22 17:10:36 +0000

    auto build

--- .automation/build-all.sh
+++ .automation/build-all.sh
@@ -6,7 +6,7 @@ cd ~/environment/tfekscode/lb2
 curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json -s
 cd $cur
 buildok=1
-dirs="tf-setup net iam c9net cluster nodeg lb2 cicd extra/nodeg2 extra/eks-cidr2 extra/sampleapp2 extra/.fargate extra/.karpenter"
+dirs="tf-setup net iam c9net cluster nodeg cicd lb2 extra/nodeg2 extra/eks-cidr2 extra/sampleapp2 extra/.fargate extra/.karpenter"
 for i in `echo $dirs`;do
     ./build-stage.sh $i 2>&1 | tee -a build.log
     grep Error: build.log
