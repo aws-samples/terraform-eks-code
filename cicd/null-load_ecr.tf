8661de2bb9b7c07f52b342f62df27cdd604c5a67
awsandy <awsandy@amazon.co.uk>
2022-03-09 17:54:15 +0000

    karpenter

--- cicd/null-load_ecr.tf
+++ cicd/null-load_ecr.tf
@@ -8,14 +8,15 @@ provisioner "local-exec" {
     when = create
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        echo "Karpenter ..."
-        docker pull public.ecr.aws/karpenter/controller:v${var.karpenter_version}
-        docker tag public.ecr.aws/karpenter/controller:v${var.karpenter_version} $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/controller:v${var.karpenter_version}
-        docker pull public.ecr.aws/karpenter/webhook:v${var.karpenter_version}
-        docker tag public.ecr.aws/karpenter/webhook:v${var.karpenter_version} $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/webhook:v${var.karpenter_version}
-        docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/webhook:v${var.karpenter_version}
-        docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/controller:v${var.karpenter_version}
-        ./load_ecr.sh
+        ./load_ecr.sh ${var.karpenter_version}
+        #echo "Karpenter ..."
+        #docker pull public.ecr.aws/karpenter/controller:v${var.karpenter_version}
+        #docker tag public.ecr.aws/karpenter/controller:v${var.karpenter_version} $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/controller:v${var.karpenter_version}
+        #docker pull public.ecr.aws/karpenter/webhook:v${var.karpenter_version}
+        #docker tag public.ecr.aws/karpenter/webhook:v${var.karpenter_version} $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/webhook:v${var.karpenter_version}
+        #docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/webhook:v${var.karpenter_version}
+        #docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/controller:v${var.karpenter_version}
+
         echo "************************************************************************************"
      EOT
 }
