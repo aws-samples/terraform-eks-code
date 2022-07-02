9dc72b9964cf10ce5d01948c294985a0679f06fc
awsandy <awsandy@amazon.co.uk>
2022-07-02 09:23:45 +0100

    terraform version to 1.2.4

--- tf-setup/gen-backend.sh
+++ tf-setup/gen-backend.sh
@@ -35,7 +35,7 @@ do
     # write out the backend config 
     printf "" > $of
     printf "terraform {\n" >> $of
-    printf "required_version = \"~> 1.1.0\"\n" >> $of
+    printf "required_version = \"~> 1.2.4\"\n" >> $of
     printf "required_providers {\n" >> $of
     printf "  aws = {\n" >> $of
     printf "   source = \"hashicorp/aws\"\n" >> $of
