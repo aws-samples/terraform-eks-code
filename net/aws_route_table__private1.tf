81f5f0727f6e0e83f69bb9e72505ba729760ade3
awsandy <awsandy@amazon.co.uk>
2022-09-24 16:39:57 +0100

    bump versions

--- net/aws_route_table__private1.tf
+++ net/aws_route_table__private1.tf
@@ -9,14 +9,15 @@ resource "aws_route_table" "rtb-0fe5311ff86579482" {
       destination_prefix_list_id = ""
       egress_only_gateway_id     = ""
       gateway_id                 = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
+      instance_id                = null
+      ipv6_cidr_block            = null
       local_gateway_id           = ""
       nat_gateway_id             = aws_nat_gateway.nat-cicd.id
       network_interface_id       = ""
       transit_gateway_id         = ""
       vpc_peering_connection_id  = ""
       vpc_endpoint_id            = ""
+      core_network_arn = ""
     },
   ]
   tags = {
--- net/aws_route_table__public1.tf
+++ net/aws_route_table__public1.tf
@@ -9,14 +9,15 @@ resource "aws_route_table" "rtb-0c9df3106b9e1bca9" {
       destination_prefix_list_id = ""
       egress_only_gateway_id     = ""
       gateway_id                 = aws_internet_gateway.igw-02c295b01d50c9d3e.id
-      instance_id                = ""
-      ipv6_cidr_block            = ""
+      instance_id                = null
+      ipv6_cidr_block            = null
       local_gateway_id           = ""
       nat_gateway_id             = ""
       network_interface_id       = ""
       transit_gateway_id         = ""
       vpc_peering_connection_id  = ""
       vpc_endpoint_id            = ""
+      core_network_arn = ""
     },
   ]
   tags   = {}
