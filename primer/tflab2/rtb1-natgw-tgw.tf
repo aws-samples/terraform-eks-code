e1d91e4594296d18c29b96299239bc3f0fe22f46
awsandy <awsandy@amazon.co.uk>
2022-09-25 11:32:07 +0100

    bump versions

--- primer/tflab2/rtb1-natgw-tgw.tf
+++ primer/tflab2/rtb1-natgw-tgw.tf
@@ -8,8 +8,8 @@ resource "aws_route_table" "rtb1-natgw-tgw" {
       cidr_block                = "0.0.0.0/0"
       egress_only_gateway_id    = ""
       gateway_id                = ""
-      instance_id               = ""
-      ipv6_cidr_block           = ""
+      instance_id               = null
+      ipv6_cidr_block           = null
       nat_gateway_id            = aws_nat_gateway.mynatgw[count.index].id
       network_interface_id      = ""
       transit_gateway_id        = ""
@@ -18,6 +18,7 @@ resource "aws_route_table" "rtb1-natgw-tgw" {
       vpc_endpoint_id = ""
       carrier_gateway_id = ""
       destination_prefix_list_id = ""
+      core_network_arn = ""
     },
   ]
   tags   = {}
--- primer/tflab2/rtb2-igw.tf
+++ primer/tflab2/rtb2-igw.tf
@@ -8,8 +8,8 @@ resource "aws_route_table" "rtb2-igw" {
       cidr_block                = "0.0.0.0/0"
       egress_only_gateway_id    = ""
       gateway_id                = aws_internet_gateway.myigw[count.index].id
-      instance_id               = ""
-      ipv6_cidr_block           = ""
+      instance_id               = null
+      ipv6_cidr_block           = null
       nat_gateway_id            = ""
       network_interface_id      = ""
       transit_gateway_id        = ""
@@ -18,6 +18,7 @@ resource "aws_route_table" "rtb2-igw" {
       "vpc_endpoint_id" = ""
       carrier_gateway_id = ""
       destination_prefix_list_id = ""
+      core_network_arn = ""
     },
   ]
   tags   = {}
