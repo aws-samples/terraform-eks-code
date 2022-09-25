095745cc2c09a588781dd588be61a40ee9ea4b1f
awsandy <awsandy@amazon.co.uk>
2022-09-25 15:26:05 +0100

    redo backend

--- tf-setup/remote-cluster.tf
+++ tf-setup/remote-cluster.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "cluster" {
 
   backend = "s3"
   config = {
-    bucket = format("tf-eks-state-%s", var.tfid)
+    bucket = format("tf-state-workshop-%s", var.tfid)
     region = data.aws_region.current.name
     key    = "terraform/tf_state_cluster.tfstate"
   }
