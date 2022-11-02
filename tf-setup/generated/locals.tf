db39348d74a8da5c4793ba935b319070bcf8852d
awsandy <awsandy@amazon.co.uk>
2022-11-02 10:59:02 +0000

    locals

--- tf-setup/generated/locals.tf
+++ tf-setup/generated/locals.tf
@@ -2,5 +2,5 @@ locals {
   BUCKET_NAME = "tf-state-workshop-${data.aws_ssm_parameter.tf-eks-id.value}"  
   BKEY="terraform/terraform_locks_${lower(basename(path.cwd))}.tfstate"
   BREG="${data.aws_region.current.name}"
-  DTAB="terraform_locks_${${lower(basename(path.cwd))}}"
+  DTAB="terraform_locks_${lower(basename(path.cwd))}"
 }
