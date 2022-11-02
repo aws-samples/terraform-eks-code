fb43b087f385bdfa0a07893ce800dfaf4bbe777a
awsandy <awsandy@amazon.co.uk>
2022-11-02 08:15:09 +0000

    params intro

--- tf-setup/data-params.tf
+++ tf-setup/data-params.tf
@@ -1,10 +1,9 @@
 data "aws_ssm_parameter" "tf-eks-id" {
   name        = "/workshop/tf-eks/id"
-
+}
 
 data "aws_ssm_parameter" "tf-eks-keyid" {
   name        = "/workshop/tf-eks/keyid"
-
 }
 
 data "aws_ssm_parameter" "tf-eks-region" {
