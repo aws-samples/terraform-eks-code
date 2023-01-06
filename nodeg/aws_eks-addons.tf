35c06f46f582604f1cb9ee69f7196d115cc1f473
awsandy <awsandy@amazon.co.uk>
2023-01-06 17:05:49 +0000

    add-ons

--- nodeg/aws_eks-addons.tf
+++ nodeg/aws_eks-addons.tf
@@ -1,10 +1,22 @@
 
-# Don't manage CNI - as custom config will be lost !
-#resource "aws_eks_addon" "vpc-cni" {
-#  depends_on     = [aws_eks_node_group.ng1]
-#  cluster_name = data.aws_eks_cluster.eks_cluster.name
-#  addon_name   = "vpc-cni"
-#}
+# CNI helm values here: https://github.com/aws/amazon-vpc-cni-k8s/blob/master/charts/aws-vpc-cni/values.yaml
+
+locals {
+  cni_config = file("${path.module}/cni.json")
+}
+
+
+resource "aws_eks_addon" "vpc-cni" {
+  depends_on     = [aws_eks_node_group.ng1]
+  cluster_name = data.aws_eks_cluster.eks_cluster.name
+  addon_name   = "vpc-cni"
+  resolve_conflicts = "OVERWRITE"
+
+  configuration_values = local.cni_config
+
+  preserve = true
+
+}
 
 resource "aws_eks_addon" "kube-proxy" {
   depends_on     = [aws_eks_node_group.ng1]
