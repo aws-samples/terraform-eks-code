c9d8bd34a036de677700e727200ebbf04f8fd537
awsandy <awsandy@amazon.co.uk>
2023-01-06 21:02:35 +0000

    sub fix

---
+++ nodeg/data-subnet-i.tf
@@ -0,0 +1,13 @@
+data "aws_subnet" "i1" {
+  id = data.terraform_remote_state.net.outputs.sub-isol1
+}
+
+data "aws_subnet" "i2" {
+  id = data.terraform_remote_state.net.outputs.sub-isol2
+
+}
+
+data "aws_subnet" "i3" {
+  id = data.terraform_remote_state.net.outputs.sub-isol3
+  
+}
