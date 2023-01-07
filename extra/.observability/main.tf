b1bf1ee4713b7386525b891a3683b85fed2e64bb
awsandy <awsandy@amazon.co.uk>
2023-01-07 10:36:22 +0000

    ob

--- extra/.observability/main.tf
+++ extra/.observability/main.tf
@@ -2,4 +2,6 @@ module "eks_observability_accelerator" {
   source = "github.com/aws-observability/terraform-aws-observability-accelerator?ref=v1.5.0"
   aws_region = data.aws_region.current.name
   eks_cluster_id = data.aws_ssm_parameter.tf-eks-cluster-name.value
+  enable_managed_prometheus = true
+  enable_managed_grafana       = true
 }
