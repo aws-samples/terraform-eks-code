5438ace1d5059f002bb9dfad549ad9a64bef994f
awsandy <awsandy@amazon.co.uk>
2023-01-07 10:30:55 +0000

    ob

--- extra/.observability/main.tf
+++ extra/.observability/main.tf
@@ -1,5 +1,5 @@
 module "eks_observability_accelerator" {
   source = "github.com/aws-observability/terraform-aws-observability-accelerator?ref=v1.5.0"
   aws_region = data.aws_region.current.name
-  eks_cluster_id = "my-eks-cluster"
+  eks_cluster_id = data.aws_ssm_parameter.tf-eks-cluster-name.value
 }
