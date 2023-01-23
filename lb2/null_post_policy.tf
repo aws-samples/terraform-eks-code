aab55e56cd5f92cec346aa9ee7d38277f29bdac6
awsandy <awsandy@amazon.co.uk>
2023-01-23 18:25:46 +0000

    up

--- lb2/null_post_policy.tf
+++ lb2/null_post_policy.tf
@@ -13,9 +13,9 @@ provisioner "local-exec" {
         cn=$(echo ${data.aws_eks_cluster.eks_cluster.name})
         echo "$reg $cn $acc"
         ./post-policy.sh $reg $cn $acc
-        echo "reannotate nodes"
-        cd ../eks-cidr
-        ./reannotate-nodes.sh $cn
+        #echo "reannotate nodes"
+        #cd ../eks-cidr
+        #./reannotate-nodes.sh $cn
         echo "done"
      EOT
 }
