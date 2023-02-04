807bfdc09445a36fa22e4891e72fc17431da8b9b
awsandy <awsandy@amazon.co.uk>
2023-02-04 18:25:53 +0000

    up

--- iam-profile.sh
+++ iam-profile.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
-rolen="eksworkshop-admin2"
-profile_name="eksworkshop-admin2"
+rolen="eksworkshop-admin"
+profile_name="eksworkshop-admin"
 aws iam create-role --role-name $rolen --assume-role-policy-document file://trust.json
-aws iam create-policy --policy-name tfeks2  --policy-document file://policy.json
-parn=$(aws iam list-policies --scope Local | jq -r '.Policies[] | select(.PolicyName=="tfeks2").Arn')
-aws iam attach-role-policy --role-name eksworkshop-admin2 --policy-arn arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore
-aws iam attach-role-policy --role-name eksworkshop-admin2 --policy-arn $parn
-profile_name="eksworkshop-admin2"
+aws iam create-policy --policy-name tfeks  --policy-document file://policy.json
+parn=$(aws iam list-policies --scope Local | jq -r '.Policies[] | select(.PolicyName=="tfeks").Arn')
+aws iam attach-role-policy --role-name $rolen --policy-arn arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore
+aws iam attach-role-policy --role-name $rolen --policy-arn $parn
 aws iam create-instance-profile --instance-profile-name $profile_name
 aws iam add-role-to-instance-profile --instance-profile-name $profile_name --role-name $rolen
 instance_id=$(curl -sS http://169.254.169.254/latest/meta-data/instance-id)
