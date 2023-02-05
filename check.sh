f4eb4df4de41ba89bb1e6c396d0388164ffbc275
awsandy <awsandy@amazon.co.uk>
2023-02-05 15:07:08 +0000

    up

--- check.sh
+++ check.sh
@@ -5,11 +5,10 @@ ip=`aws ec2 describe-iam-instance-profile-associations --filters "Name=instance-
 echo "Instance Profile=$ip"
 if [ "$ip" != "AWSCloud9SSMInstanceProfile" ] ; then
 echo "ERROR: Could not find Instance profile AWSCloud9SSMAccessRole! - DO NOT PROCEED"
-echo "Check Cloud9 AWS Managed temporary credentials are disabled - in AWS Settings"
 exit
 else
 echo "PASSED: Found Instance profile - proceed with the workshop"
 fi
 aws sts get-caller-identity --query Arn | grep AWSCloud9SSMAccessRole -q && echo "PASSED: IAM role valid" || echo "ERROR: IAM role not valid - DO NOT PROCEED"
 iname=$(aws ec2 describe-tags --filters "Name=resource-type,Values=instance" "Name=resource-id,Values=$instid" | jq -r '.Tags[] | select(.Key=="Name").Value')
-echo $iname| grep 'eks-terraform\|-Project-mod-' -q && echo "PASSED: Cloud9 IDE name is valid" || echo "ERROR: Cloud9 IDE name invalid! - DO NOT PROCEED"
+echo $iname| grep 'eks-terraform\|-Project-mod-' -q && echo "PASSED: Cloud9 IDE name is valid" || echo "ERROR: Cloud9 IDE name invalid! - DO NOT PROCEED" && echo "Check Cloud9 AWS Managed temporary credentials are disabled - in AWS Settings"
