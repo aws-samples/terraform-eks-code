fb8a7998db3c8b02846384637e467f98be7db90d
awsandy <awsandy@amazon.co.uk>
2023-02-07 21:45:15 +0000

    up

--- nodeg/data-eks-cluster.tf
+++ nodeg/data-eks-cluster.tf
@@ -2,20 +2,4 @@ data "aws_eks_cluster" "eks_cluster" {
   name = data.aws_ssm_parameter.cluster-name.value
 }
 
-output "endpoint" {
-  value = data.aws_eks_cluster.eks_cluster.endpoint
-}
-
-output "ca" {
-  value = data.aws_eks_cluster.eks_cluster.certificate_authority[0].data
-}
-
-# Only available on Kubernetes version 1.13 and 1.14 clusters created or upgraded on or after September 3, 2019.
-output "identity-oidc-issuer" {
-  value = data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
-}
-
-output "cluster-name" {
-  value = data.aws_eks_cluster.eks_cluster.name
-}
 
