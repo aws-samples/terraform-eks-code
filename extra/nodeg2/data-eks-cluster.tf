6da9b09b5e0c185f37f2dcdba601c06266ab0ed0
awsandy <awsandy@amazon.co.uk>
2023-02-08 11:03:50 +0000

    up

--- extra/nodeg2/data-eks-cluster.tf
+++ extra/nodeg2/data-eks-cluster.tf
@@ -1,3 +1,3 @@
 data "aws_eks_cluster" "eks_cluster" {
-  name = data.aws_ssm_parameter.cluster-name.value
+  name =  nonsensitive(data.aws_ssm_parameter.cluster-name.value)
 }
--- nodeg/data-eks-cluster.tf
+++ nodeg/data-eks-cluster.tf
@@ -1,5 +1,5 @@
 data "aws_eks_cluster" "eks_cluster" {
-  name = data.aws_ssm_parameter.cluster-name.value
+  name =  nonsensitive(data.aws_ssm_parameter.cluster-name.value)
 }
 
 
