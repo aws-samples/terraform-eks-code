7c146768220d3a698e24c01a970686ae94325f6a
awsandy <awsandy@amazon.co.uk>
2023-04-09 12:31:11 +0100

    quay

--- cicd/ecr-pull-through.tf
+++ cicd/ecr-pull-through.tf
@@ -2,3 +2,8 @@ resource "aws_ecr_pull_through_cache_rule" "aws" {
   ecr_repository_prefix = "aws"
   upstream_registry_url = "public.ecr.aws"
 }
+
+resource "aws_ecr_pull_through_cache_rule" "quay" {
+  ecr_repository_prefix = "quay"
+  upstream_registry_url = "quay.io"
+}
