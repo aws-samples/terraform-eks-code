5e990ff8db61a660c3c9e01da5b5ff3ff3b3fdc4
awsandy <awsandy@amazon.co.uk>
2023-04-15 18:11:53 +0100

    up

--- .bastion/sg-rule.tf
+++ .bastion/sg-rule.tf
@@ -3,6 +3,6 @@ resource "aws_security_group_rule" "eks-all" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = [data.aws_vpc.vpc-default.cidr_block]
+  cidr_blocks       = [data.aws_ssm_parameter.eks-vpc.value]
   security_group_id = data.aws_ssm_parameter.net-cluster-sg.value
 }
