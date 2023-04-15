fe2a568554ee98c03096d65e01ea9214d1100d4e
awsandy <awsandy@amazon.co.uk>
2023-04-15 18:26:29 +0100

    up

--- .bastion/c9.tf
+++ .bastion/c9.tf
@@ -5,4 +5,13 @@ resource "aws_cloud9_environment_ec2" "bastion" {
   connection_type = "CONNECT_SSM"
   image_id = "amazonlinux-2-x86_64"
   subnet_id = data.aws_ssm_parameter.sub-priv1.value
+  # = format("arn:aws:sts::%s:assumed-role/WSParticipantRole/Participant",data.aws_caller_identity.current.account_id)
+
+}
+
+
+resource "aws_cloud9_environment_membership" "bastion" {
+  environment_id = aws_cloud9_environment_ec2.bastion.id
+  permissions    = "read-write"
+  user_arn       =  format("arn:aws:sts::%s:assumed-role/WSParticipantRole/Participant",data.aws_caller_identity.current.account_id)
 }
