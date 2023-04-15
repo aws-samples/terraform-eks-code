81137df37028d69bffeabc893928c782f7af7a83
awsandy <awsandy@amazon.co.uk>
2023-04-15 18:05:21 +0100

    c9 sg

--- .bastion/c9.tf
+++ .bastion/c9.tf
@@ -4,5 +4,5 @@ resource "aws_cloud9_environment_ec2" "bastion" {
   automatic_stop_time_minutes = "120"
   connection_type = "CONNECT_SSM"
   image_id = "amazonlinux-2-x86_64"
-  subnet_id = data.aws_ssm_parameter.sub-isol1.value
+  subnet_id = data.aws_ssm_parameter.sub-priv1.value
 }
