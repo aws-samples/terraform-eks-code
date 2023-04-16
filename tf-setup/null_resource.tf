200bd35dfbcee483e26982c1af8f70ded72e6154
awsandy <awsandy@amazon.co.uk>
2023-04-16 13:53:05 +0100

    up

--- tf-setup/null_resource.tf
+++ tf-setup/null_resource.tf
@@ -5,7 +5,7 @@ resource "null_resource" "gen_backend" {
   depends_on = [aws_dynamodb_table.terraform_locks,aws_s3_bucket_server_side_encryption_configuration.terraform_state]
   provisioner "local-exec" {
     when    = create
-    command = = <<EOT
+    command = <<EOT
         sleep 6
         ./gen-backend.sh
     EOT
