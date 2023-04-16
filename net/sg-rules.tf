41dd83196d3f0591f96e95985c9d0cebe4486fce
awsandy <awsandy@amazon.co.uk>
2023-04-16 13:56:29 +0100

    up

--- net/sg-rules.tf
+++ net/sg-rules.tf
@@ -3,6 +3,6 @@ resource "aws_security_group_rule" "eks-ssl" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.eks-vpc.cidr_block]
+  cidr_blocks       = [aws_vpc.cluster.cidr_block]
   security_group_id = aws_security_group.cluster-sg.id
 }
