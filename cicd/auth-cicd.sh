33ad1c48ff22ed8e7daf9817041c182085d61ab3
awsandy <awsandy@amazon.co.uk>
2023-04-16 17:07:33 +0100

    up

--- cicd/auth-cicd.sh
+++ cicd/auth-cicd.sh
@@ -1,5 +1,7 @@
 test -n "$ACCOUNT_ID" && echo ACCOUNT_ID is "$ACCOUNT_ID" || "echo ACCOUNT_ID is not set && exit"
-ROLE="    - rolearn: arn:aws:iam::$ACCOUNT_ID:role/codebuild-eks-cicd-build-app-service-role\n      username: build\n      groups:\n        - system:masters"
+# get the workshop id from SSM
+WSID=$(aws ssm get-parameter --name /workshop/tf-eks/id --query Parameter.Value --output text)
+ROLE="    - rolearn: arn:aws:iam::$ACCOUNT_ID:role/$WSID-codebuild-eks-service-role\n      username: build\n      groups:\n        - system:masters"
 #
 kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapRoles: \|/{print;print \"$ROLE\";next}1" > /tmp/aws-auth-patch.yml
 #
