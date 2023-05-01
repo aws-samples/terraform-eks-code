f7e9230873b18bfea6f0bfc05ac2207ba83f40c5
awsandy <awsandy@amazon.co.uk>
2023-05-01 21:02:30 +0100

    up

--- sampleapp/sampleapp-ingress.tf
+++ sampleapp/sampleapp-ingress.tf
@@ -1,5 +1,6 @@
 resource "kubernetes_ingress_v1" "game-2048__ingress-2048" {
-  #wait_for_load_balancer = true
+  wait_for_load_balancer = true
+  depends_on = [kubernetes_namespace.game-2048]
   metadata {
     annotations = { "alb.ingress.kubernetes.io/scheme" = "internal", "alb.ingress.kubernetes.io/target-type" = "ip", "alb.ingress.kubernetes.io/listen-ports" = "[{\"HTTP\": 8080}]" }
     name        = "ingress-2048"
@@ -24,3 +25,12 @@ resource "kubernetes_ingress_v1" "game-2048__ingress-2048" {
     }
   }
 }
+
+output "load_balancer_hostname" {
+  value = kubernetes_ingress_v1.game-2048__ingress-2048.status.0.load_balancer.0.ingress.0.hostname
+}
+
+# Display load balancer IP (typically present in GCP, or using Nginx ingress controller)
+#output "load_balancer_ip" {
+#  value = kubernetes_ingress_v1.game-2048__ingress-2048.status.0.load_balancer.0.ingress.0.ip
+#}
