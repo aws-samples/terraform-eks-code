e83a086394f8257e803d0d4a658b3b9d27a29f0b
awsandy <awsandy@amazon.co.uk>
2023-05-03 19:15:30 +0100

    up

--- lb2/helm_loadbalancer.tf
+++ lb2/helm_loadbalancer.tf
@@ -29,7 +29,7 @@ resource "helm_release" "aws-load-balancer-controller" {
 
   set {
     name  = "image.tag"
-    value = "v2.5.0"
+    value = "v2.5.1"
   }
 
 # https://github.com/kubernetes-sigs/aws-load-balancer-controller/issues/3035
