550efc5aa5117514b1f22286fdcb90060c849941
awsandy <awsandy@amazon.co.uk>
2023-06-27 12:53:27 +0100

    c9 auth

--- nodeg/c9-auth.sh
+++ nodeg/c9-auth.sh
@@ -22,5 +22,3 @@ EOF
 kubectl get configmap -n kube-system aws-auth -o yaml > aws-auth.yaml
 cat patch.yaml >> aws-auth.yaml
 kubectl apply -f aws-auth.yaml
-#eksctl create iamidentitymapping --cluster mycluster1 --arn ${rolearn} --group system:masters --username admin
-kubectl describe configmap -n kube-system aws-auth
