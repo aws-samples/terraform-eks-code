f2baf2815411706a373530d3d950d9a091ccd4ac
awsandy <awsandy@amazon.co.uk>
2023-06-29 21:32:10 +0100

    up

--- cicd/ecr-pull-through.tf
+++ cicd/ecr-pull-through.tf
@@ -3,7 +3,3 @@ resource "aws_ecr_pull_through_cache_rule" "aws" {
   upstream_registry_url = "public.ecr.aws"
 }
 
-#resource "aws_ecr_pull_through_cache_rule" "quay" {
-#  ecr_repository_prefix = "quay"
-#  upstream_registry_url = "quay.io"
-#}
--- cicd/null-load_ecr.tf
+++ cicd/null-load_ecr.tf
@@ -9,14 +9,6 @@ resource "null_resource" "load_ecr" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
         ./load_ecr.sh ${var.karpenter_version}
-        #echo "Karpenter ..."
-        #docker pull public.ecr.aws/karpenter/controller:v${var.karpenter_version}
-        #docker tag public.ecr.aws/karpenter/controller:v${var.karpenter_version} $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/controller:v${var.karpenter_version}
-        #docker pull public.ecr.aws/karpenter/webhook:v${var.karpenter_version}
-        #docker tag public.ecr.aws/karpenter/webhook:v${var.karpenter_version} $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/webhook:v${var.karpenter_version}
-        #docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/webhook:v${var.karpenter_version}
-        #docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/controller:v${var.karpenter_version}
-
         echo "************************************************************************************"
      EOT
   }
