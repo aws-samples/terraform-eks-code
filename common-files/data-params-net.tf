d13203c35277878f62426f5d356971610ba51402
awsandy <awsandy@amazon.co.uk>
2023-12-17 10:10:41 +0000

    up

--- common-files/data-params-net.tf
+++ common-files/data-params-net.tf
@@ -6,11 +6,33 @@ data "aws_ssm_parameter" "eks-cidr" {
   name        = "/workshop/tf-eks/eks-cidr"
 }
 
-
-
 data "aws_ssm_parameter" "cicd-cidr" {
   name        = "/workshop/tf-eks/cicd-cidr"
 }
+
+data "aws_ssm_parameter" "cicd-vpc" {
+  name        = "/workshop/tf-eks/cicd-vpc"
+}
+
+data "aws_ssm_parameter" "private_subnets" {
+  name        = "/workshop/tf-eks/private_subnets"
+}  
+
+data "aws_ssm_parameter" "intra_subnets" {
+  name        = "/workshop/tf-eks/intra_subnets"
+}  
+
+data "aws_ssm_parameter" "private_rtb" {
+  name        = "/workshop/tf-eks/private_rtb"
+} 
+
+data "aws_ssm_parameter" "intra_rtb" {
+  name        = "/workshop/tf-eks/intra_rtb"
+} 
+
+data "aws_ssm_parameter" "public_rtb" {
+  name        = "/workshop/tf-eks/public_rtb"
+} 
 
 
 
