d7a1b233e9bc57f681aca991bc77990d38b2f2f7
awsandy <awsandy@amazon.co.uk>
2023-12-17 10:16:51 +0000

    up

--- c9net/eks-route-add.tf
+++ c9net/eks-route-add.tf
@@ -1,48 +1,12 @@
 resource "aws_route" "rt-eks1" {
-  route_table_id            = data.aws_ssm_parameter.rtb-priv1.value
-  destination_cidr_block    = data.aws_vpc.vpc-default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
-}
-
-resource "aws_route" "rt-eks2" {
-  route_table_id            = data.aws_ssm_parameter.rtb-priv2.value
-  destination_cidr_block    = data.aws_vpc.vpc-default.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
-}
-
-resource "aws_route" "rt-eks3" {
-  route_table_id            = data.aws_ssm_parameter.rtb-priv3.value
+  route_table_id            = data.aws_ssm_parameter.private_rtb.value
   destination_cidr_block    = data.aws_vpc.vpc-default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
 }
 
 resource "aws_route" "rt-eks-isol" {
-  route_table_id            = data.aws_ssm_parameter.rtb-isol.value
+  route_table_id            = data.aws_ssm_parameter.intra_rtb.value
   destination_cidr_block    = data.aws_vpc.vpc-default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
 }
 
-
-resource "aws_route" "rt-eks4" {
-  route_table_id            = data.aws_ssm_parameter.rtb-priv1.value
-  destination_cidr_block    = data.aws_vpc.vpc-cicd.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.cicd-peer.id
-}
-
-resource "aws_route" "rt-eks5" {
-  route_table_id            = data.aws_ssm_parameter.rtb-priv2.value
-  destination_cidr_block    = data.aws_vpc.vpc-cicd.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.cicd-peer.id
-}
-
-resource "aws_route" "rt-eks6" {
-  route_table_id            = data.aws_ssm_parameter.rtb-priv3.value
-  destination_cidr_block    = data.aws_vpc.vpc-cicd.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.cicd-peer.id
-}
-
-resource "aws_route" "rt-cicd-isol" {
-  route_table_id            = data.aws_ssm_parameter.rtb-isol.value
-  destination_cidr_block    = data.aws_vpc.vpc-cicd.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.cicd-peer.id
-}
