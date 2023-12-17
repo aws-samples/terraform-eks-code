a62a4c87091275df58bfde4b65436b5eda1fbbdd
awsandy <awsandy@amazon.co.uk>
2023-12-17 11:22:44 +0000

    up

--- cluster/data-eks.tf
+++ cluster/data-eks.tf
@@ -9,13 +9,13 @@ data "aws_eks_cluster" "example" {
   name = module.eks.cluster_name
 }
 
-output "endpoint" {
-  value = data.aws_eks_cluster.example.endpoint
-}
+#output "endpoint" {
+#  value = data.aws_eks_cluster.example.endpoint
+#}
 
 # Only available on Kubernetes version 1.13 and 1.14 clusters created or upgraded on or after September 3, 2019.
-output "identity-oidc-issuer" {
-  value = data.aws_eks_cluster.example.identity[0].oidc[0].issuer
-}
+#output "identity-oidc-issuer" {
+#  value = data.aws_eks_cluster.example.identity[0].oidc[0].issuer
+#}
 
 
