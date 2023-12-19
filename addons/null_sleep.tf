8734bbf05067addb51c2c62f8306c7d942c07c5a
awsandy <awsandy@amazon.co.uk>
2023-12-19 16:35:31 +0000

    up

--- addons/null_sleep.tf
+++ addons/null_sleep.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "sleep" {
   triggers = {
-    always_run = timestamp()
-    depends_on=[module.eks_blueprints_addons.aws_load_balancer_controller]
+    always_run = timestamp() 
   }
+  depends_on=[module.eks_blueprints_addons.aws_load_balancer_controller]
   provisioner "local-exec" {
     on_failure  = fail
     when        = create
