313bd674210c0a048a7f7da5ac17529f9d90253b
awsandy <awsandy@amazon.co.uk>
2023-12-22 12:02:47 +0000

    up

--- ssl-demo/04-r53.sh
+++ ssl-demo/04-r53.sh
@@ -1,13 +1,10 @@
 vpcid=$(aws ssm get-parameter --name /workshop/tf-eks/eks-vpc --query Parameter.Value --output text)
 aws route53 create-hosted-zone --name rabbit.local \
---caller-reference my-private-zone\
---hosted-zone-config Comment=”my private zone”,PrivateZone=true \
---vpc VPCRegion=eu-west-1,VPCId=$vpcid
+--caller-reference my-private-zone \
+--hosted-zone-config Comment=”my private zone”,PrivateZone=true --vpc VPCRegion=eu-west-1,VPCId=$vpcid
 aws route53 create-hosted-zone --name hamster.local \
---caller-reference my-private-zone\
---hosted-zone-config Comment=”my private zone”,PrivateZone=true \
---vpc VPCRegion=eu-west-1,VPCId=$vpcid
+--caller-reference my-private-zone \
+--hosted-zone-config Comment=”my private zone”,PrivateZone=true --vpc VPCRegion=eu-west-1,VPCId=$vpcid
 aws route53 create-hosted-zone --name chipmunk.local \
---caller-reference my-private-zone\
---hosted-zone-config Comment=”my private zone”,PrivateZone=true \
---vpc VPCRegion=eu-west-1,VPCId=$vpcid
+--caller-reference my-private-zone \
+--hosted-zone-config Comment=”my private zone”,PrivateZone=true --vpc VPCRegion=eu-west-1,VPCId=$vpcid
