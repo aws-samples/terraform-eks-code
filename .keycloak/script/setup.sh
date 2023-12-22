5769de6e10afca919fad1051454e541c3c5068c8
awsandy <awsandy@amazon.co.uk>
2023-12-22 14:46:32 +0000

    up

--- .keycloak/script/setup.sh
+++ .keycloak/script/setup.sh
@@ -1,5 +1,12 @@
-export PDNS=example.com
-export HOSTED_ZONE=$PDNS
+openssl req -new -x509 -sha256 -nodes -newkey rsa:4096 -keyout private_keycloak.key -out certificate_keycloak.crt -subj "/CN=keycloak.local"
+aws acm import-certificate --certificate fileb://certificate_keycloak.crt --private-key fileb://private_keycloak.key
+vpcid=$(aws ssm get-parameter --name /workshop/tf-eks/eks-vpc --query Parameter.Value --output text)
+aws route53 create-hosted-zone --name keycloak.local \
+--caller-reference my-keycloak-zone \
+--hosted-zone-config Comment="keycloak local",PrivateZone=true --vpc VPCRegion=eu-west-1,VPCId=$vpcid
+keyz=$(aws route53 list-hosted-zones | jq -r '.HostedZones[] | select(.Name=="keycloak.local.").Id' | cut -f3 -d'/')
+export ACM_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='keycloak.local'].CertificateArn" --include keyTypes=RSA_4096 --output text)
+export HOSTED_ZONE=keycloak.local
 export ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 #export HOSTED_ZONE=$ACCOUNT_ID.awsandy.people.aws.dev
 export KEYCLOAK_PASSWORD="keycloakpass123"
@@ -7,7 +14,7 @@ export WORKSPACE_ENDPOINT=$(aws grafana list-workspaces --query 'workspaces[0].e
 echo $WORKSPACE_ENDPOINT
 echo $KEYCLOAK_PASSWORD
 export WORKSPACE_ID=$(aws grafana list-workspaces --query 'workspaces[0].id' --output text)
-export SAML_URL=https://keycloak.$HOSTED_ZONE/realms/keycloak-blog/protocol/saml/descriptor
+export SAML_URL=https://keycloak.local/realms/keycloak-blog/protocol/saml/descriptor
 cat << EOF > keycloak_values.yaml
 auth:
   adminUser: admin
@@ -109,7 +116,9 @@ ingress:
     alb.ingress.kubernetes.io/scheme: internet-facing
     alb.ingress.kubernetes.io/target-type: 'ip'
     alb.ingress.kubernetes.io/listen-ports: '[{"HTTPS":443}]'
-  hostname: keycloak.${HOSTED_ZONE}
+    alb.ingress.kubernetes.io/ssl-redirect: '443'
+    alb.ingress.kubernetes.io/certificate-arn: ${ACM_ARN}
+  hostname: keycloak.local
 EOF
 echo "Installing Keycloak"
 helm repo add bitnami https://charts.bitnami.com/bitnami
