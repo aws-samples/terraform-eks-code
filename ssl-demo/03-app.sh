dd4a9f18d8564d061756adf46967f840aff6eac8
awsandy <awsandy@amazon.co.uk>
2023-12-22 13:48:57 +0000

    up

--- ssl-demo/03-app.sh
+++ ssl-demo/03-app.sh
@@ -1,13 +1,13 @@
 export CURRET_PET=rabbit
 export ACM_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='rabbit.local'].CertificateArn" --include keyTypes=RSA_4096 --output text)
 envsubst < "manifest-template.yaml" > "rabbit.yaml"
-export CURRET_PET=chipmunk
-export ACM_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='chipmunk.local'].CertificateArn" --include keyTypes=RSA_4096 --output text)
-envsubst < "manifest-template.yaml" > "chipmunk.yaml"
-export CURRET_PET=hamster
-export ACM_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='hamster.local'].CertificateArn" --include keyTypes=RSA_4096 --output text)
-envsubst < "manifest-template.yaml" > "hamster.yaml"
-kubectl create ns myapplications-ns
-kubectl apply -f ./hamster.yaml 
-kubectl apply -f ./chipmunk.yaml 
+#export CURRET_PET=chipmunk
+#export ACM_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='chipmunk.local'].CertificateArn" --include keyTypes=RSA_4096 --output text)
+#envsubst < "manifest-template.yaml" > "chipmunk.yaml"
+#export CURRET_PET=hamster
+#export ACM_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='hamster.local'].CertificateArn" --include keyTypes=RSA_4096 --output text)
+#envsubst < "manifest-template.yaml" > "hamster.yaml"
+#kubectl create ns myapplications-ns
+#kubectl apply -f ./hamster.yaml 
+#kubectl apply -f ./chipmunk.yaml 
 kubectl apply -f ./rabbit.yaml
--- ssl-demo/04-r53.sh
+++ ssl-demo/04-r53.sh
@@ -2,10 +2,10 @@ vpcid=$(aws ssm get-parameter --name /workshop/tf-eks/eks-vpc --query Parameter.
 aws route53 create-hosted-zone --name rabbit.local \
 --caller-reference my-rabbit-zone \
 --hosted-zone-config Comment="rabbit local",PrivateZone=true --vpc VPCRegion=eu-west-1,VPCId=$vpcid
-aws route53 create-hosted-zone --name hamster.local \
---caller-reference my-hamster-zone \
---hosted-zone-config Comment="hamster local",PrivateZone=true --vpc VPCRegion=eu-west-1,VPCId=$vpcid
-aws route53 create-hosted-zone --name chipmunk.local \
---caller-reference my-chipmunk-zone \
---hosted-zone-config Comment="chipmunk local",PrivateZone=true --vpc VPCRegion=eu-west-1,VPCId=$vpcid
+#aws route53 create-hosted-zone --name hamster.local \
+#--caller-reference my-hamster-zone \
+#--hosted-zone-config Comment="hamster local",PrivateZone=true --vpc VPCRegion=eu-west-1,VPCId=$vpcid
+#aws route53 create-hosted-zone --name chipmunk.local \
+#--caller-reference my-chipmunk-zone \
+#--hosted-zone-config Comment="chipmunk local",PrivateZone=true --vpc VPCRegion=eu-west-1,VPCId=$vpcid
 
