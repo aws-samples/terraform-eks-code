dc701bc8e043b388983952638ace73a1f11dd8d7
awsandy <awsandy@amazon.co.uk>
2024-01-18 09:44:54 +0000

    up

--- .keycloak/script/cleanup.sh
+++ .keycloak/script/cleanup.sh
@@ -1,2 +1,3 @@
+export ACM_ARN=$(aws acm list-certificates --query "CertificateSummaryList[?DomainName=='keycloak.testdomain.local'].CertificateArn" --include keyTypes=RSA_2048 --output text)
 kubectl delete ns keycloak
-#aws acm delete-certificate
+aws acm delete-certificate --certificate-arn $ACM_ARN
