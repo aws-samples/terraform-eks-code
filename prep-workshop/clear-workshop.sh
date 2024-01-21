15baf3163191f9868530896798368e61d1455f26
awsandy <awsandy@amazon.co.uk>
2024-01-21 15:45:14 +0000

    up

--- prep-workshop/clear-workshop.sh
+++ prep-workshop/clear-workshop.sh
@@ -1,5 +1,8 @@
 cd ~/environment
 time eksctl delete cluster  --name eks-workshop     # ~8min
+#
+# Why dekete these ? - EFS so can zap VPC (has eni)
+#
 fsid=$(aws efs describe-file-systems --query FileSystems[].FileSystemId --output text)
 for mtid in $(aws efs describe-mount-targets --file-system-id $fsid --query MountTargets[].MountTargetId --output text);do
 echo $mtid
@@ -8,5 +11,8 @@ done
 echo "sleep 30s to allow mount targets to delete"
 sleep 30
 aws efs delete-file-system --file-system-id $fsid
+#
+# Same for RDS
+#
 dbi=$(aws rds describe-db-instances --query DBInstances[].DBInstanceIdentifier --output text)
 aws rds delete-db-instance --db-instance-identifier $dbi --skip-final-snapshot
