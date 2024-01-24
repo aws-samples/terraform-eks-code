9a2894dea87aea14aff6ff2e23b312242411c6f4
awsandy <awsandy@amazon.co.uk>
2024-01-24 18:27:27 +0000

    up

--- .keycloak/database/06-config-keycloak.sh
+++ .keycloak/database/06-config-keycloak.sh
@@ -1,7 +1,10 @@
 # problem addressing  keycloak.testdomain.local
 #
-kchn="keycloak.testdomain.local"
+export ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+#kchn="keycloak.testdomain.local"
+kchn=$(echo keycloak.$ACCOUNT_ID.awsandy.people.aws.dev)
 export KEYCLOAK_PASSWORD="keycloakpass123"
+echo $kchn
 com=`printf "docker run \
     -e KEYCLOAK_URL="http://%s:8080/" \
     -e KEYCLOAK_USER="admin" \
