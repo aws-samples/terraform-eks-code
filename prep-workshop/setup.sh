b89f6365a28917e9aa8f0401b787a13ef0798ae7
awsandy <awsandy@amazon.co.uk>
2024-01-31 19:04:11 +0000

    up

--- prep-workshop/setup.sh
+++ prep-workshop/setup.sh
@@ -19,4 +19,13 @@ flux --version
 #curl -L -o kubectl-cert-manager.tar.gz https://github.com/jetstack/cert-manager/releases/latest/download/kubectl-cert_manager-linux-amd64.tar.gz
 #tar xzf kubectl-cert-manager.tar.gz
 #sudo mv kubectl-cert_manager /usr/local/bin
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+export TF_VAR_region=${AWS_REGION}
+test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo "AWS_REGION is not set !!"
+echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
+echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+echo "export TF_VAR_region=${AWS_REGION}" | tee -a ~/.bash_profile
+aws configure set default.region ${AWS_REGION}
+aws configure get region
 
