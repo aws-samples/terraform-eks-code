a02a95bace650dad0f15e598e4dd41f1efdf0084
awsandy <awsandy@amazon.co.uk>
2024-02-10 18:31:33 +0000

    up

--- .keycloak/install-keycloack.sh
+++ .keycloak/install-keycloack.sh
@@ -1,6 +1,9 @@
 # validate dns exit of not valid
-
-
-# install cert
-
+acc=$(aws sts get-caller-identity --query Account --output text)
+dnsl=$(dig $acc.awsandy.people.aws.dev NS +short | wc -l)
+if [[ $dnsl -gt 0 ]]; then
+    # install cert
+    terraform init
+    terraforn apply -auto-approve
 # prep config
+fi
