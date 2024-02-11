d0f7759625ebe5b8e64915e47c7155c66d9389e9
awsandy <awsandy@amazon.co.uk>
2024-02-11 10:00:40 +0000

    up

--- .keycloak/install-keycloack.sh
+++ .keycloak/install-keycloack.sh
@@ -27,7 +27,6 @@ if [[ $? -eq 0 ]];then
     kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=keycloak -n keycloak  --timeout=30s
     kubectl port-forward -n keycloak svc/keycloak 8080:8080 > /dev/null 2>&1 &
     pid=$!
-
     # Default token expires in one minute. May need to extend. very ugly
     KEYCLOAK_TOKEN=$(curl -sS  --fail-with-body -X POST -H "Content-Type: application/x-www-form-urlencoded" \
     --data-urlencode "username=cnoe-admin" \
@@ -35,7 +34,7 @@ if [[ $? -eq 0 ]];then
     --data-urlencode "grant_type=password" \
     --data-urlencode "client_id=admin-cli" \
     localhost:8080/realms/master/protocol/openid-connect/token | jq -e -r '.access_token')
-    
+
     curl -sS -H "Content-Type: application/json"   -H "Authorization: bearer ${KEYCLOAK_TOKEN}"   -X POST --data @config-payloads/realm.json   localhost:8080/admin/realms
     curl -sS -H "Content-Type: application/json"   -H "Authorization: bearer ${KEYCLOAK_TOKEN}"   -X POST --data @config-payloads/users.json   localhost:8080/admin/realms/keycloak-blog/users
     curl -sS -H "Content-Type: application/json"   -H "Authorization: bearer ${KEYCLOAK_TOKEN}"   -X POST --data @config-payloads/client.json   localhost:8080/admin/realms/keycloak-blog/clients
