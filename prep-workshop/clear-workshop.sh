32e1ed9f92903fe9b8998ba7cd9a53df486fb706
awsandy <awsandy@amazon.co.uk>
2024-02-17 12:26:33 +0000

    up

--- prep-workshop/clear-workshop.sh
+++ prep-workshop/clear-workshop.sh
@@ -1,13 +1,13 @@
 cd ~/environment
 delete-environment
-time eksctl delete cluster --name eks-workshop     # ~8min
+time eksctl delete cluster --name eks-workshop # ~8min
 #
 # Why delete these ? - EFS so can zap VPC (has eni)
 #
 fsid=$(aws efs describe-file-systems --query FileSystems[].FileSystemId --output text)
-for mtid in $(aws efs describe-mount-targets --file-system-id $fsid --query MountTargets[].MountTargetId --output text);do
-echo $mtid
-aws efs delete-mount-target --mount-target-id $mtid
+for mtid in $(aws efs describe-mount-targets --file-system-id $fsid --query MountTargets[].MountTargetId --output text); do
+    echo $mtid
+    aws efs delete-mount-target --mount-target-id $mtid
 done
 echo "sleep 30s to allow mount targets to delete"
 sleep 30
@@ -23,16 +23,22 @@ vpcid=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=eksctl-eks-workshop
 echo $vpcid
 
 sgs=$(aws ec2 describe-security-groups --filters Name=vpc-id,Values=$vpcid --query SecurityGroups[].GroupId --output text)
-for i in $sgs;do 
-echo $i
-aws ec2 delete-security-group --group-id $i
+for i in $sgs; do
+    echo $i
+    aws ec2 delete-security-group --group-id $i
 done
-for i in $sgs;do 
-echo $i
-aws ec2 delete-security-group --group-id $i
+for i in $sgs; do
+    echo $i
+    aws ec2 delete-security-group --group-id $i
 done
-echo "await RDS deletion then"
-#aws rds describe-db-instances --db-instance-identifier eks-workshop-catalog --query 'DBInstances[].DBInstanceStatus' --output text
-echo "aws ec2 delete-vpc --vpc-id $vpcid"
+echo "await RDS deletion"
+rdsst=$(aws rds describe-db-instances --db-instance-identifier eks-workshop-catalog --query 'DBInstances[].DBInstanceStatus' --output text)
+while [[ $rdsst != "" ]]; do
+    echo $rdsst
+    sleep 10
+    rdsst=$(aws rds describe-db-instances --db-instance-identifier eks-workshop-catalog --query 'DBInstances[].DBInstanceStatus' --output text)
+done
+echo "delete vpc --vpc-id $vpcid"
 aws ec2 delete-vpc --vpc-id $vpcid
+echo "done"
 #
