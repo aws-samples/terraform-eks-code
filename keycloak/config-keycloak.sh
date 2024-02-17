5cca9662f8c6076f93b2d6c208847a183efb9f6e
awsandy <awsandy@amazon.co.uk>
2024-02-17 17:53:06 +0000

    up

--- keycloak/config-keycloak.sh
+++ keycloak/config-keycloak.sh
@@ -1,6 +1,6 @@
 echo "start port forwarding for local config"
-kubectl port-forward -n keycloak svc/keycloak '8080:80' &
-pid=$!
+com="kubectl port-forward -n keycloak svc/keycloak 8080:80 &"
+eval $com
 # Default token expires in one minute. May need to extend. very ugly
 KEYCLOAK_TOKEN=$(curl -sS --fail-with-body -X POST -H "Content-Type: application/x-www-form-urlencoded" \
     --data-urlencode "username=admin" \
@@ -11,4 +11,5 @@ KEYCLOAK_TOKEN=$(curl -sS --fail-with-body -X POST -H "Content-Type: application
 curl -sS -H "Content-Type: application/json" -H "Authorization: bearer ${KEYCLOAK_TOKEN}" -X POST --data @config-payloads/realm.json localhost:8080/admin/realms
 curl -sS -H "Content-Type: application/json" -H "Authorization: bearer ${KEYCLOAK_TOKEN}" -X POST --data @config-payloads/users.json localhost:8080/admin/realms/keycloak-blog/users
 curl -sS -H "Content-Type: application/json" -H "Authorization: bearer ${KEYCLOAK_TOKEN}" -X POST --data @config-payloads/client.json localhost:8080/admin/realms/keycloak-blog/clients
+pid=$(ps -ef | grep port-forward | grep -v grep | awk '{print $2}')
 kill $pid
