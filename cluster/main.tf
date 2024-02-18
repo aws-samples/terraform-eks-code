5d8066554507d927d00f2244507091e383b9aac1
awsandy <awsandy@amazon.co.uk>
2024-02-18 13:43:22 +0000

    up

--- cluster/main.tf
+++ cluster/main.tf
@@ -219,7 +219,7 @@ resource "helm_release" "karpenter" {
   repository_password = data.aws_ecrpublic_authorization_token.token.password
   chart               = "karpenter"
   # v0.33+ goes to beta apis - might break things !!
-  version             = "v0.31.2"
+  version             = "v0.31.4"
 
   set {
     name  = "settings.aws.clusterName"
