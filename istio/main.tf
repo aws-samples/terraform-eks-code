613a1d02b314be5fb5e10a2cb6ba246731dee02c
awsandy <awsandy@amazon.co.uk>
2024-02-21 21:37:22 +0000

    up

--- istio/main.tf
+++ istio/main.tf
@@ -1,4 +1,17 @@
 # istio via Terraform
+
+provider "kubernetes" {
+  host                   = data.aws_ssm_parameter.endpoint.value
+  cluster_ca_certificate = base64decode(data.aws_ssm_parameter.ca.value)
+  config_path = "~/.kube/config"
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", data.aws_ssm_parameter.cluster-name.value]
+  }
+}
+
 provider "helm" {
   kubernetes {
     host                   = data.aws_ssm_parameter.endpoint.value
@@ -12,6 +25,21 @@ provider "helm" {
     }
   }
 }
+
+provider "kubectl" {
+  apply_retry_count      = 5
+  host                   = data.aws_ssm_parameter.endpoint.value
+  cluster_ca_certificate = base64decode(data.aws_ssm_parameter.ca.value)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", data.aws_ssm_parameter.cluster-name.value]
+  }
+}
+
 
 locals {
   istio_charts_url = "https://istio-release.storage.googleapis.com/charts"
