525bcdb0bdbbcc60e3cd83a2f2159aab19ea30e3
awsandy <awsandy@amazon.co.uk>
2024-02-21 21:41:01 +0000

    up

--- common-files/aws.tf
+++ common-files/aws.tf
@@ -23,6 +23,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.12.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
     local = {
       source  = "hashicorp/local"
       version = "~> 2.4.0"
