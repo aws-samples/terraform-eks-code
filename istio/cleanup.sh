c5873f7bc4bcf4e4bc92c01f3a1de68a22fc05f1
awsandy <awsandy@amazon.co.uk>
2024-02-25 13:48:03 +0000

    up

--- istio/cleanup.sh
+++ istio/cleanup.sh
@@ -2,4 +2,5 @@ cd ~/environment/istio-1.20.3
 kubectl delete -f samples/addons
 kubectl delete -f samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
+cd ~/environment/tfekscode/istio
 terraform destroy -auto-approve
