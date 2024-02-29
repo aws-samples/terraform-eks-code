cf64260c1da41825e1a642f9048ec7dd5bb3f306
awsandy <awsandy@amazon.co.uk>
2024-02-29 18:54:23 +0000

    up

---
+++ istio/null_resource.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "restart" {
+  triggers = {
+    always_run = timestamp()
+  }
+  depends_on = [eks_blueprints_addons]
+  provisioner "local-exec" {
+    on_failure  = fail
+    when        = create
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOT
+       kubectl rollout restart deployment istio-ingress -n istio-ingress
+     EOT
+  }
+}
