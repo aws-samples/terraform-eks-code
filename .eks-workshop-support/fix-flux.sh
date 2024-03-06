5cf15649b246c095e2dccb3cb688d9a29bc59d96
awsandy <awsandy@amazon.co.uk>
2024-03-06 22:42:30 +0000

    up

--- .eks-workshop-support/fix-flux.sh
+++ .eks-workshop-support/fix-flux.sh
@@ -1,6 +1,2 @@
 flux uninstall -s
 sudo curl -s https://fluxcd.io/install.sh | sudo FLUX_VERSION=2.1.0 bash
-#echo "Lab cleanup ....."
-#~/environment/tfekscode/.eks-workshop-support/clean-fluxlab.sh 2> /dev/null
-#kubectl delete ns ui
-#kubectl apply -k ~/environment/eks-workshop/base-application
