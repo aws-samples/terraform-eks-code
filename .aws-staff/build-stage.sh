41aee9f1a1d6ff011f3a38ca0263fdc74ed71390
awsandy <awsandy@amazon.co.uk>
2024-03-15 15:00:52 +0000

    up

--- .aws-staff/build-stage.sh
+++ .aws-staff/build-stage.sh
@@ -18,26 +18,25 @@ for i in $dirs; do
     #terraform init -no-color -force-copy > /dev/null
     terraform init -no-color >/dev/null
     rc=0
-    tobuild=$(terraform plan -json -out tfplan | jq '.changes' | grep -v null | jq .add | tail -1)
-    echo "tobuild = $tobuild"
+    terraform plan -json -out tfplan > tfplan.json
+    tobuild=$(cat tfplan.json | jq '.changes' | grep -v null | jq .add | tail -1)
+    toremove=$(cat tfplan.json | jq '.changes' | grep -v null | jq .remove | tail -1)
+    echo "tobuild = $tobuild  toremove=$toremove"
     if [[ $tobuild -gt 0 ]]; then
-        terraform state list 2>/dev/null | grep aws_ >/dev/null
-        if [ $? -eq 0 ]; then
-            rc=$(terraform state list | grep aws_ | wc -l)
-        fi
-        if [ $rc -ge $tobuild ]; then echo "$rc in tf state expected $tobuild so skipping build ..." && continue; fi
-
+        
+        #if [[ $tobuild != $toremove ]]
         # array elements in here so special rule
 
         #echo "Terraform Plan"
         #pc=$(terraform plan -json -out tfplan | jq '.changes' | grep -v null | jq .add | tail -1)
-        terraform apply tfplan -no-color -json
+        terraform apply tfplan -no-color -json > apply.json
+        rc=$(cat apply.json | jq '.changes' | grep -v null | jq .add | tail -1)
 
         #terraform apply tfplan -no-color && terraform init -force-copy -no-color
         #echo "State to S3. ."
         #terraform init -force-copy -no-color
 
-        rc=$(terraform state list | grep aws_ | wc -l)
+        #rc=$(terraform state list | grep aws_ | wc -l)
 
     # double check the helm chart has gone in
     fi
