1c15fb3b74b2549f30c71ccc8184e414d31761b8
awsandy <awsandy@amazon.co.uk>
2024-03-22 15:12:29 +0000

    up

---
+++ prep-workshop/bootstrap.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/aws-samples/eks-workshop-v2/main/lab/scripts/installer.sh
+wget https://raw.githubusercontent.com/aws-samples/eks-workshop-v2/main/lab/scripts/setup.sh
+chmod +x installer.sh
+chmod +x setup.sh
+sudo ./installer.sh
+./setup.sh
