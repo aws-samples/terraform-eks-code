939043a8361f84f6fc4c9e3864d6e11a769919fc
awsandy <awsandy@amazon.co.uk>
2024-05-07 11:18:56 +0100

    up

--- cluster/do-karpenter.sh
+++ cluster/do-karpenter.sh
@@ -1,4 +1,4 @@
 export CLUSTER_NAME=$(aws eks list-clusters --query clusters[0] | tr -d '"')
-export KAR_ROLE=$(aws iam list-roles --query Roles[].RoleName | grep Karpenter-eks-workshop | tr -d ' |"')
+export KAR_ROLE=$(aws iam list-roles --query Roles[].RoleName | grep Karpenter-eks-workshop | tr -d ' |"|,')
 envsubst < karpenter-node_class.yaml | kubectl apply -f -
 envsubst < karpenter-node_pool.yaml | kubectl apply -f -
