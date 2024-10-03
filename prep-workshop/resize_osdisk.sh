47afd20f9266aea45c92f5cd609155fa810dcaeb
awsandy <awsandy@amazon.co.uk>
2024-10-03 17:58:34 +0100

    up

---
+++ prep-workshop/resize_osdisk.sh
@@ -0,0 +1,46 @@
+# ------  resize OS disk -----------
+# Specify the desired volume size in GiB as a command-line argument. If not specified, default to 32 GiB.
+VOLUME_SIZE=${1:-32}
+
+# Get the ID of the environment host Amazon EC2 instance.
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data//instance-id)
+
+# Get the ID of the Amazon EBS volume associated with the instance.
+VOLUME_ID=$(aws ec2 describe-instances \
+  --instance-id $INSTANCE_ID \
+  --query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId" \
+  --output text)
+
+# Resize the EBS volume.
+aws ec2 modify-volume --volume-id $VOLUME_ID --size $VOLUME_SIZE > /dev/null
+
+# Wait for the resize to finish.
+while [ \
+  "$(aws ec2 describe-volumes-modifications \
+    --volume-id $VOLUME_ID \
+    --filters Name=modification-state,Values="optimizing","completed" \
+    --query "length(VolumesModifications)"\
+    --output text)" != "1" ]; do
+sleep 1
+done
+
+if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
+then
+  # Rewrite the partition table so that the partition takes up all the space that it can.
+  sudo growpart /dev/xvda 1
+ 
+  # Expand the size of the file system.
+  sudo resize2fs /dev/xvda1 > /dev/null
+
+else
+  # Rewrite the partition table so that the partition takes up all the space that it can.
+  sudo growpart /dev/nvme0n1 1
+
+  # Expand the size of the file system.
+  sudo resize2fs /dev/nvme0n1p1 &> /dev/null #(Amazon Linux 1)
+  sudo xfs_growfs /dev/nvme0n1p1 &> /dev/null #(Amazon Linux 2)
+fi
+df -m /
+
+
+
