f1da4c2bbd86b0c2b413b8b8f87f844298dc465d
awsandy <awsandy@amazon.co.uk>
2024-10-04 16:45:49 +0100

    up

--- cluster/karpenter-node_class.tf
+++ cluster/karpenter-node_class.tf
@@ -1,11 +1,11 @@
 resource "kubectl_manifest" "karpenter_node_class" {
   yaml_body = <<-YAML
-    apiVersion: karpenter.k8s.aws/v1beta1
+    apiVersion: karpenter.k8s.aws/v1
     kind: EC2NodeClass
     metadata:
       name: default
     spec:
-      amiFamily: AL2
+      amiFamily: AL2023
       role: ${module.karpenter.node_iam_role_name}
       subnetSelectorTerms:
         - tags:
