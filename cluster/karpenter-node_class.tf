9478dd7aaad0fc6a25738fd5360b45c30b94a43b
awsandy <awsandy@amazon.co.uk>
2024-10-04 16:55:37 +0100

    up

--- cluster/karpenter-node_class.tf
+++ cluster/karpenter-node_class.tf
@@ -15,6 +15,11 @@ resource "kubectl_manifest" "karpenter_node_class" {
             karpenter.sh/discovery: ${module.eks.cluster_name}
       tags:
         karpenter.sh/discovery: ${module.eks.cluster_name}
+      amiSelectorTerms:
+    #    - id: "${ARM_AMI_ID}"
+    #    - id: "${AMD_AMI_ID}"
+    #   - id: "${GPU_AMI_ID}" # <- GPU Optimized AMD AMI 
+        - name: "amazon-eks-node-${data.aws_ssm_parameter.tf-eks-version.value}-*" # <- automatically upgrade when a new AL2 EKS Optimized AMI is released. This is unsafe for production workloads. Validate AMIs in lower environments before deploying them to production.
   YAML
 
   depends_on = [
