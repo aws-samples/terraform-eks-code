96df5eec399f188494ec350241039914942d026a
Andy Thomas <awsandy@amazon.co.uk>
2024-10-24 18:40:13 +0100

    yp

---
+++ .aws-staff/del-karpenter.sh
@@ -0,0 +1,7 @@
+# kubectl logs -l app.kubernetes.io/instance=karpenter -n kube-system | jq '.'
+kubectl delete nodepool default
+kubectl delete ec2nodeclass default
+kubectl -n kube-system scale deployment karpenter --replicas 0
+sleep 5 
+kubectl -n kube-system scale deployment karpenter --replicas 2
+terraform apply -replace="kubectl_manifest.karpenter_node_class"
