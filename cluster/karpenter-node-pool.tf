23ef66a01e8cbbe67fd1b34d290ef05d135f62c1
Andy Thomas <awsandy@amazon.co.uk>
2024-10-30 21:51:37 +0000

    up

--- cluster/karpenter-node-pool.tf
+++
@@ -1,4 +0,0 @@
-resource "kubectl_manifest" "karpenter_node_pool" {
-  yaml_body = file("${path.module}/karpenter-node_pool.yaml")
-  depends_on = [kubectl_manifest.karpenter_node_class]
-}
--- cluster/null_auth.tf
+++ cluster/null_auth.tf
@@ -10,14 +10,16 @@ resource "null_resource" "gen_cluster_auth" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
         CLUSTER_NAME=$(echo ${data.aws_ssm_parameter.tf-eks-cluster-name.value})
+        KAR_ROLE=$(echo ${module.karpenter.node_iam_role_name})
         echo "Cluster name = $CLUSTER_NAME"
         aws eks update-kubeconfig --name $CLUSTER_NAME
         #eksctl utils write-kubeconfig --cluster $CLUSTER_NAME
         #context=$(kubectl config get-contexts -o name | grep $CLUSTER_NAME)
         ##kubectl config rename-context $context $CLUSTER_NAME
         kubectl version && kubectl get nodes
-        #sleep 5
-        #./do-karpenter.sh
+        sleep 2
+        envsubst < karpenter-node_class.yaml | kubectl apply -f -
+        envsubst < karpenter-node_pool.yaml | kubectl apply -f -
      EOT
   }
 }
