9946fd365f8541d2372016f6a1a35a3edda1f9b9
Andy Thomas <awsandy@amazon.co.uk>
2024-11-01 09:44:47 +0000

    up

--- cluster/null_auth.tf
+++ cluster/null_auth.tf
@@ -9,9 +9,10 @@ resource "null_resource" "gen_cluster_auth" {
     when        = create
     interpreter = ["/bin/bash", "-c"]
     command     = <<EOT
-        CLUSTER_NAME=$(echo ${data.aws_ssm_parameter.tf-eks-cluster-name.value})
-        KAR_ROLE=$(echo ${module.karpenter.node_iam_role_name})
+        CLUSTER_NAME=$(echo ${nonsensitive(data.aws_ssm_parameter.tf-eks-cluster-name.value)})
+        KAR_ROLE=$(echo ${nonsensitive(module.karpenter.node_iam_role_name)})
         echo "Cluster name = $CLUSTER_NAME"
+        echo "KAR_ROLE = $KAR_ROLE"
         aws eks update-kubeconfig --name $CLUSTER_NAME
         #eksctl utils write-kubeconfig --cluster $CLUSTER_NAME
         #context=$(kubectl config get-contexts -o name | grep $CLUSTER_NAME)
