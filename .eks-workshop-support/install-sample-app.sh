7a2751cfca7bc9ddc0b9eef9898b1b02b1fd0a32
Andy Thomas <awsandy@amazon.co.uk>
2024-11-01 15:12:45 +0000

    up

--- .eks-workshop-support/install-sample-app.sh
+++ .eks-workshop-support/install-sample-app.sh
@@ -4,5 +4,4 @@ kubectl wait --for=condition=Ready pods --all -n catalog --timeout=180s
 kubectl apply -k ~/environment/eks-workshop/base-application
 kubectl wait --for=condition=Ready --timeout=180s pods -l app.kubernetes.io/created-by=eks-workshop -A
 kubectl get deployment -l app.kubernetes.io/created-by=eks-workshop -A
-
-
+cp ingress.yaml ~/environment/eks-workshop/modules/exposing/ingress/creating-ingress
