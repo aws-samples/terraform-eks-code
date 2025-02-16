a819ac28a79b64c7803c404dbde404ba8b0ec297
Andy Thomas <awsandy@amazon.co.uk>
2025-02-16 19:21:44 +0000

    up

--- cluster/main.tf
+++ cluster/main.tf
@@ -11,17 +11,6 @@ resource "aws_ec2_instance_metadata_defaults" "metadata" {
   instance_metadata_tags      = "disabled"
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
-}
 
 
 data "aws_availability_zones" "available" {}
