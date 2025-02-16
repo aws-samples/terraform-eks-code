206fdf3ed8ef6e0ce5d2e35b4210f9909eddeb71
Andy Thomas <awsandy@amazon.co.uk>
2025-02-16 22:37:00 +0000

    up

--- net/sg-def-rule-eks.tf
+++ net/sg-def-rule-eks.tf
@@ -13,14 +13,8 @@ resource "aws_default_security_group" "default" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["::/0"]
-  }
-}
 
 
