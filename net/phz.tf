fa0c67fdc832813b35679ce96b8abfddf7a09942
Andy Thomas <awsandy@amazon.co.uk>
2025-02-19 19:17:32 +0000

    up

--- net/phz.tf
+++ net/phz.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "keycloak" {
-  name = format("%s.%s.%s",data.aws_caller_identity.current.account_id,data.aws_ssm_parameter.tf-eks-id.value)
+  name = format("%s.%s.%s",data.aws_caller_identity.current.account_id,data.aws_ssm_parameter.tf-eks-id.value,var.dn)
   
 }
 
