92a16e50debb218619c27415ff66e9f7995bc772
Andy Thomas <awsandy@amazon.co.uk>
2025-02-21 19:17:26 +0000

    up

--- .aws-staff/build-all.sh
+++ .aws-staff/build-all.sh
@@ -18,10 +18,10 @@ date >> build.log
 
 set +e # turn off error checking - proceed if error
 echo "Some post build verifications"
-echo "Should have at least 23 pods running in total"
+echo "Should have at least 21 pods running in total"
 rc=$(kubectl get pods -A | grep Running | wc -l)
-if [ $rc -lt 23 ]; then 
-echo "ERROR: Found only $rc pods running - expected 23"
+if [ $rc -lt 21 ]; then 
+echo "ERROR: Found only $rc pods running - expected 21"
 else
 echo "PASSED: running pod count $rc"
 fi
