ba143b5b2b88ee523809501e08c74831f691214b
Andy Thomas <awsandy@amazon.co.uk>
2025-02-21 17:15:38 +0000

    up

--- net/eks-route-add.tf
+++ net/eks-route-add.tf
@@ -4,6 +4,19 @@ resource "aws_route" "rt-eks1" {
   vpc_peering_connection_id = aws_vpc_peering_connection.def-peer.id
 }
 
+resource "aws_route" "nat-ipv6" {
+  route_table_id            = aws_ssm_parameter.private_rtb.value
+  destination_cidr_block    = "::/0"
+  vpc_peering_connection_id = module.vpc.aws_nat_gateway.this[0].id
+}
+
+
+resource "aws_route" "nat-ipv4" {
+  route_table_id            = aws_ssm_parameter.private_rtb.value
+  destination_cidr_block    = "0.0.0.0/0"
+  vpc_peering_connection_id = module.vpc.aws_nat_gateway.this[0].id
+}
+
 resource "aws_route" "rt-eks-isol" {
   route_table_id            = aws_ssm_parameter.intra_rtb.value
   destination_cidr_block    = data.aws_vpc.vpc-default.cidr_block
