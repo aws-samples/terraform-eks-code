ddf6ace0dac4f8d59f5d0986ad1b331f723e82b8
Andy Thomas <awsandy@amazon.co.uk>
2025-02-21 17:29:25 +0000

    up

--- net/eks-route-add.tf
+++ net/eks-route-add.tf
@@ -12,7 +12,7 @@ resource "aws_route" "nat-ipv6" {
 
 resource "aws_route" "nat-ipv4" {
   route_table_id            = aws_ssm_parameter.private_rtb.value
-  destination_ipv6_cidr_block    = "0.0.0.0/0"
+  destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = module.vpc.natgw_ids[0]
 }
 
