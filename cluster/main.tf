b632f60151ab6c211dd1dea0e95904b5544ceed9
Andy Thomas <awsandy@amazon.co.uk>
2025-02-22 18:24:05 +0000

    up

--- cluster/main.tf
+++ cluster/main.tf
@@ -107,20 +107,3 @@ module "kms" {
 
   tags = local.tags
 }
-
-
-
-resource "aws_iam_role_policy_attachment" "additional_node_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-  role       = module.eks.node_iam_role_arn
-}
-
-
-#AmazonInspector2ManagedCispolicy = "arn:aws:iam::aws:policy/AmazonInspector2ManagedCisPolicy"
-
-#Not for auto
-
-#AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore" - not for auto
-#AmazonEKSWorkerNodePolicy = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
