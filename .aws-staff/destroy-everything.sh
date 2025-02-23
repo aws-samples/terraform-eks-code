c9a308d304cb634aaad5fa069587e94c7bba578b
Andy Thomas <awsandy@amazon.co.uk>
2025-02-23 12:54:30 +0000

    up

--- .aws-staff/destroy-everything.sh
+++ .aws-staff/destroy-everything.sh
@@ -80,9 +80,6 @@ for i in $dirs; do
         #kubextl delete deployment ebs-csi-controller -n kube-system
         #terraform destroy -target module.eks.aws_eks_addon.aws-ebs-csi-driver -auto-approve
         #terraform destroy -target amazon-cloudwatch-observability -auto-approve
-        terraform destroy -target helm_release.karpenter -auto-approve
-        echo "EKS Managed Node Group delete ~9m"
-        terraform destroy -target module.eks.module.eks_managed_node_group -auto-approve # gets addons too
         echo "EKS Cluster delete ~3m"
         terraform destroy -target module.eks.aws_eks_cluster.this -auto-approve
         terraform destroy -target module.eks -auto-approve
