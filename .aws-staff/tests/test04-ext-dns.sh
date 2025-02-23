d4b47b07ccb9a849a2cd29880b096d9eaa1a515a
Andy Thomas <awsandy@amazon.co.uk>
2025-02-23 10:14:50 +0000

    up

--- .aws-staff/tests/test04-ext-dns.sh
+++ .aws-staff/tests/test04-ext-dns.sh
@@ -71,8 +71,8 @@ spec:
 EOF
 
 # 4. Wait for service to get LoadBalancer IP/hostname
-echo "Waiting for LoadBalancer to be ready (sleep 180) ..."
-sleep 180
+echo "Waiting for LoadBalancer to be ready (sleep 240) ..."
+sleep 240
 kubectl wait --for=jsonpath='{.status.loadBalancer.ingress[0]}' service nginx-test -n external-dns-test --timeout=180s || {
     echo "Error: LoadBalancer not ready"
     exit 1
