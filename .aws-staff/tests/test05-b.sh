963daa782a858665b753da41da8b13333cafe00e
Andy Thomas <awsandy@amazon.co.uk>
2025-02-23 17:31:56 +0000

    up

--- .aws-staff/tests/test05-b.sh
+++ .aws-staff/tests/test05-b.sh
@@ -1,16 +1,18 @@
 echo -n 'KEYID' > ./access-key
 echo -n 'SECRETKEY' > ./secret-access-key
-kubectl create secret generic awssm-secret --from-file=./access-key --from-file=./secret-access-key
+kubectl create namespace test-ext-secret
+kubectl -n test-ext-secret create secret generic awssm-secret --from-file=./access-key --from-file=./secret-access-key
 cat << EOF | kubectl apply -f -
 apiVersion: external-secrets.io/v1beta1
 kind: SecretStore
 metadata:
   name: secretstore-sample
+  namespace: test-ext-secret
 spec:
   provider:
     aws:
       service: SecretsManager
-      region: eu-west-2
+      region: eu-west-1
       auth:
         secretRef:
           accessKeyIDSecretRef:
