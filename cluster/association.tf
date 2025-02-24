667128cda118589583d43f74aec2f88e19bd43a9
Andy Thomas <awsandy@amazon.co.uk>
2025-02-24 21:29:43 +0000

    up

--- cluster/association.tf
+++ cluster/association.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_access_entry" "automode_node" {
-  cluster_name  =# module.eks.cluster_name
+  cluster_name  = module.eks.cluster_name
   principal_arn = module.eks.node_iam_role_arn
   type          = "EC2"
 }
