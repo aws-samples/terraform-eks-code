6a5b6e68ebe2c2148a8c4016fd77b1d893b85f1d
Andy Thomas <awsandy@amazon.co.uk>
2025-02-24 17:32:55 +0000

    up

---
+++ cluster/nodepool.tf
@@ -0,0 +1,40 @@
+resource "kubectl_manifest" "karpenter_node_class" {
+yaml_body = <<-YAML
+apiVersion: karpenter.sh/v1
+kind: NodePool
+metadata:
+  name: my-node-pool
+spec:
+  template:
+    metadata:
+      labels:
+        billing-team: my-team
+    spec:
+      nodeClassRef:
+        group: eks.amazonaws.com
+        kind: NodeClass
+        name: default
+
+      requirements:
+        - key: "eks.amazonaws.com/instance-category"
+          operator: In
+          values: ["c", "m", "r"]
+        - key: "eks.amazonaws.com/instance-cpu"
+          operator: In
+          values: ["4", "8", "16", "32"]
+        - key: "topology.kubernetes.io/zone"
+          operator: In
+          values: ["eu-west-1a", "eu-west-1b","eu-west-1c"]
+        - key: "kubernetes.io/arch"
+          operator: In
+          values: ["arm64", "amd64"]
+        - key: eks.amazonaws.com/instance-generation
+          operator: Gt
+          values: ["4"]
+  limits:
+    cpu: "1000"
+    memory: 1000Gi
+YAML
+}
+
+
