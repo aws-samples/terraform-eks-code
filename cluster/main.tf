d0f367e87c65086052f5f0eebae8ca238c149693
Andy Thomas <awsandy@amazon.co.uk>
2025-02-24 17:36:04 +0000

    up

--- cluster/main.tf
+++ cluster/main.tf
@@ -71,7 +71,7 @@ module "eks" {
 
   cluster_compute_config = {
     enabled    = true
-    node_pools = ["general-purpose","system"]
+    node_pools = ["general-purpose"]
   }
 
 # External encryption key
