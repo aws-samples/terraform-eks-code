992b1ae255b496efda2cfe3c1c0e4d93d9215d12
Andy Thomas <awsandy@amazon.co.uk>
2025-02-24 21:58:29 +0000

    up

--- cluster/nodepool.tf
+++ cluster/nodepool.tf
@@ -23,15 +23,15 @@ spec:
         - key: "eks.amazonaws.com/instance-cpu"
           operator: In
           values: ["4", "8", "16", "32"]
-        - key: "topology.kubernetes.io/zone"
-          operator: In
-          values: ["eu-west-1a", "eu-west-1b","eu-west-1c"]
+        #- key: "topology.kubernetes.io/zone"
+        #  operator: In
+        #  values: ["eu-west-1a", "eu-west-1b","eu-west-1c"]
         - key: "kubernetes.io/arch"
           operator: In
           values: ["arm64", "amd64"]
-        - key: eks.amazonaws.com/instance-generation
-          operator: Gt
-          values: ["4"]
+        #- key: eks.amazonaws.com/instance-generation
+        #  operator: Gt
+        #  values: ["4"]
   limits:
     cpu: "1000"
     memory: 1000Gi
