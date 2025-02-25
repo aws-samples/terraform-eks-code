dece086ee3fb15087d5ee7cd451bf063448a0a67
Andy Thomas <awsandy@amazon.co.uk>
2025-02-25 11:31:14 +0000

    up

--- cluster/nodepool.tf
+++ cluster/nodepool.tf
@@ -12,7 +12,7 @@ spec:
     expireAfter: 24h # 1 days
     budgets:
     - nodes: "50%"  
-    #  schedule: "0 2 * * *"  # daily at 2am UTC
+      schedule: ["0 2 * * *"]  # daily at 2am UTC
   template:
     metadata:
       labels:
