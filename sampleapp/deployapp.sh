2d81661479f17cbd39ff73756d2a9286df7f928e
Andy Thomas <awsandy@amazon.co.uk>
2025-02-27 12:20:18 +0000

    up

--- sampleapp/deployapp.sh
+++ sampleapp/deployapp.sh
@@ -1,4 +1,9 @@
-kubectl apply -f https://raw.githubusercontent.com/aws-containers/retail-store-sample-app/main/dist/kubernetes/deploy.yaml
-kubectl wait --for=condition=available deployments --all
+#kubectl apply -f https://raw.githubusercontent.com/aws-containers/retail-store-sample-app/main/dist/kubernetes/deploy.yaml
+#kubectl wait --for=condition=available deployments --all
+#sleep 5
+#kubectl get svc ui
+kubectl apply -f sampleapp.yaml -n sampleapp
+kubectl wait --for=condition=available deployments --all -n sampleapp
 sleep 5
-kubectl get svc ui
+kubectl get svc ui -n sampleapp
+ 
