1c9bbbf28ce4fe54ff01627c52a66fd51f70b9aa
Andy Thomas <awsandy@amazon.co.uk>
2025-03-01 19:34:00 +0000

    up

--- prep-workshop/installer.sh
+++ prep-workshop/installer.sh
@@ -130,6 +130,7 @@ yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/has
 yum -y install terraform
 
 echo "kubectl completion"
+mkdir -p /home/ec2-user/.bashrc.d
 /usr/local/bin/kubectl completion bash >  /home/ec2-user/.bashrc.d/kubectl_completion.bash
 echo "alias k=kubectl" >> /home/ec2-user/.bashrc.d/kubectl_completion.bash
 echo "complete -F __start_kubectl k" >> /home/ec2-user/.bashrc.d/kubectl_completion.bash
