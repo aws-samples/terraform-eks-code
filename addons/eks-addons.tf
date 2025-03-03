dae3e38a63e1d1a550f4507ec011c4b9614e0c8c
Andy Thomas <awsandy@amazon.co.uk>
2025-03-03 18:29:35 +0000

    up

--- addons/eks-addons.tf
+++ addons/eks-addons.tf
@@ -1,9 +1,9 @@
-resource "aws_eks_addon" "cloudwatch" {
+resource "aws_eks_addon" "cloudwatch-observability" {
   cluster_name                = data.aws_ssm_parameter.cluster-name.value
   addon_name                  = "amazon-cloudwatch-observability"
 }
 
-resource "aws_eks_addon" "example" {
+resource "aws_eks_addon" "metrics-server" {
   cluster_name                = data.aws_ssm_parameter.cluster-name.value
   addon_name                  = "metrics-server"
 }
