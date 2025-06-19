c64ab1f298ace7bb8c516e2631e1a93ef3868b5a
Andy Thomas <awsandy@amazon.co.uk>
2025-06-19 12:22:23 +0100

    gen backend fix

--- tf-setup/gen-backend.sh
+++ tf-setup/gen-backend.sh
@@ -55,8 +55,8 @@ do
     printf "   version = \"2.24.0\"\n" >> $of
     printf "  }\n" >> $of
     printf "  helm = {\n" >> $of
-    printf "    source  = "hashicorp/helm" \n" >> $of
-    printf "    version = "~> 2.17.0"\n" >> $of
+    printf "    source  = \"hashicorp/helm\" \n" >> $of
+    printf "    version = \"~> 2.17.0\" \n" >> $of
     printf "  }\n" >> $of
     printf "  kubectl = {\n" >> $of
     printf "    source  = \"gavinbunney/kubectl\"\n" >> $of
