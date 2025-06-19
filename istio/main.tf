33ffa2f9dcdd557ffb6a155ae1408ebd1092209a
Andy Thomas <awsandy@amazon.co.uk>
2025-06-19 11:55:29 +0100

    helm provider revert for v2

--- istio/main.tf
+++ istio/main.tf
@@ -12,7 +12,7 @@ provider "kubernetes" {
 
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = data.aws_ssm_parameter.endpoint.value
     cluster_ca_certificate = base64decode(data.aws_ssm_parameter.ca.value)
 
