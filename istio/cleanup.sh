1a0ea7d2de2a467d53b82e0359101bf1a2ed12ed
Andy Thomas <awsandy@amazon.co.uk>
2025-06-23 13:05:51 +0100

    up

--- istio/cleanup.sh
+++ istio/cleanup.sh
@@ -1,4 +1,4 @@
-cd ~/environment/istio-1.20.3
+cd ~/environment/istio-1.24.3
 kubectl delete -f samples/addons
 kubectl delete -f samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl delete -f samples/bookinfo/platform/kube/bookinfo.yaml
