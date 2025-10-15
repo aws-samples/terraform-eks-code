d169f017092fce89ef908e715a4290c1831843c9
Andy Thomas <awsandy@amazon.co.uk>
2025-10-15 14:22:35 +0100

    up

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -7,10 +7,10 @@ output "cluster_arn" {
   value       = module.eks.cluster_arn
 }
 
-output "cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = module.eks.cluster_certificate_authority_data
-}
+#output "cluster_certificate_authority_data" {
+#  description = "Base64 encoded certificate data required to communicate with the cluster"
+#  value       = module.eks.cluster_certificate_authority_data
+#}
 
 output "cluster_endpoint" {
   description = "Endpoint for your Kubernetes API server"
