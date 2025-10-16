d283cf1a3cd9df8dd7d4cc08421534877846e4c0
Andy Thomas <awsandy@amazon.co.uk>
2025-10-16 08:38:19 +0100

    up

---
+++ .aws-staff/build-to-observ.sh
@@ -0,0 +1,27 @@
+echo "circa 40 minutes ..."
+
+rm -f build.log
+date >> build.log
+buildok=1
+#orig
+dirs="tf-setup net cluster nodepool addons sampleapp observ istio"
+#set -e # turn on error checking - exit if error
+for i in `echo $dirs`;do
+    ./build-stage.sh $i 2>&1 | tee -a build.log
+    grep Error: build.log
+    if [[ $? -eq 0 ]];then
+        echo "Error: in build.log"
+        exit
+    fi
+done
+date >> build.log
+
+set +e # turn off error checking - proceed if error
+echo "Some post build verifications"
+echo "Should have at least 40 pods running in total"
+rc=$(kubectl get pods -A | grep Running | wc -l)
+if [[ $rc -lt 40 ]]; then 
+    echo "ERROR: Found only $rc pods running - expected 21"
+else
+    echo "PASSED: running pod count $rc"
+fi
--- net/vpc.tf
+++ net/vpc.tf
@@ -10,7 +10,7 @@ module "vpc" {
 
   name = local.name
   cidr = local.vpc_cidr
-  #secondary_cidr_blocks = local.secondary_cidr_blocks
+  secondary_cidr_blocks = local.secondary_cidr_blocks
 
   azs             = local.azs
  
@@ -18,9 +18,10 @@ module "vpc" {
   #private_subnets = concat(
   #  [for k, v in local.azs : cidrsubnet(element(local.secondary_cidr_blocks, 0), 2, k)]
   #)
+  private_subnets = [for k, v in local.azs : cidrsubnet(element(local.secondary_cidr_blocks, 0), 2, k)]
   
   # /22 10.0.0.0/22 and 10.0.4.0/22 and 10.0.8.0/22
-  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 6, k * 4)]
+  #private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 6, k * 4)]
   
   # 10.0.48.0/24 and 10.0.49.0/24 and 10.0.50.0/24
   public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
