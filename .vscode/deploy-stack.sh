0327a19940640bf2ec44d79b0d502d14079cec32
Andy Thomas <awsandy@amazon.co.uk>
2025-10-31 10:30:32 +0000

    up

--- .vscode/deploy-stack.sh
+++ .vscode/deploy-stack.sh
@@ -1,6 +1,7 @@
 # delete role eksworkshop-admin
 # create WSParticipantRole. 
+DEFAULT_VPC_ID=$(aws ec2 describe-vpcs --filters "Name=isDefault,Values=true" --query 'Vpcs[0].VpcId' --region eu-west-2 --output text)
 aws cloudformation create-stack --stack-name vscode \
 --template-body file://vscode-isengard.yaml --capabilities CAPABILITY_NAMED_IAM \
---parameters ParameterKey=DefaultVpcId,ParameterValue=vpc-db7c09b3 \
+--parameters ParameterKey=DefaultVpcId,ParameterValue=$DEFAULT_VPC_ID \
 --region eu-west-2
