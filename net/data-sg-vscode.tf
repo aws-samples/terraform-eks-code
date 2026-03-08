7f7dbf3b50905c4837426a6a34157f08b85eb818
Andy Thomas <awsandy@amazon.co.uk>
2026-03-08 16:09:59 +0000

    up

--- net/data-sg-vscode.tf
+++ net/data-sg-vscode.tf
@@ -1,7 +1,10 @@
 # Get the security group attached to the VSCode instance
 # sort() ensures consistent selection if multiple SGs exist
 data "aws_security_group" "c9sg" {
-  name = sort(data.aws_instance.c9inst.vpc_security_group_ids)[0]
+  filter {
+    name   = "tag:Name"
+    values = ["KiroIDE-Instance-SG"]
+  }
 }
 
 
